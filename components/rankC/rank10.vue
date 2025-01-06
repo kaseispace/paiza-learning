6d86b4e18b32717b8d9b2f284203d1b6ae87cf06
yuki0829 <fledgling0829@gmail.com>
2025-01-06 20:13:51 +0900

    feat: 練習コード

--- components/rankC/rank10.vue
+++ components/rankC/rank10.vue
@@ -1,3 +1,11 @@
 <script setup lang="ts">
-
+// 要素ではなく、indexを使用したフィルター
+const lines = [
+    "5 3", 
+    "1 2 3 4 5",
+];
+const [n, m] = lines[0].split(' ').map(Number);
+const s = lines[1].split(' ').map(Number);
+const result = s.filter((_, i) => i !== m-1 );
+result.forEach(num => console.log(num));
 </script>
