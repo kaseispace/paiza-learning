77dee5d1611006cde3278af9a3357d7a56d64600
yuki0829 <fledgling0829@gmail.com>
2025-01-09 22:06:23 +0900

    feat: 練習コード

---
+++ components/rankC/part12.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// 配列Aの要素と配列Bの要素数で得られる積の最大値
+// 求め方として以下が考えられる
+// ・比較して最大値を保持する
+    //  利点：メモリ使用量が少ない
+    //  欠点：可読性が低い場合がある
+// ・配列を作成してMath.maxを使う
+    //  利点：コードが短く、読みやすい
+    //  欠点：時的に巨大な1次元配列 a を作成するため、メモリ使用量が増加する可能性がある
+
+const lines = [
+    '10 10',
+    '85 -46 93 44 -40 -75 -75 -18 -54 95',
+    '1 95 -92 -90 32 -25 36 55 22 86',
+]
+const [n, k] = lines[0].split(' ').map(Number);
+const a = lines[1].split(' ').map(Number);
+const b = lines[2].split(' ').map(Number);
+let result = a[0] * b[0];
+// const result = [];
+  
+for(let i = 0; i < a.length; i++) {
+    for(let j = 0; j < b.length; j++) {
+        const num = a[i] * b[j];
+        // result.push(a[i] * b[j]);
+        
+        if(num > result) {
+            result = num;
+        }
+    }
+}
+</script>
+
+<template>
+    <div/>
+ </template>
---
+++ components/rankC/part13.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const lines = [100];
+const n = Number(lines[0]);
+//   n分の配列を作る
+ const sieve = Array(n + 1).fill(true);
+//  0と1は素数ではないのであらかじめfalseに設定
+ sieve[0] = sieve[1] = false;
+  
+//   与えられたnの平方根まで調べればいい
+  for(let i = 2; i <= Math.sqrt(n); i++) {
+    //   trueであれば実行、例えばiが2であった場合、2の倍数を全部falseに設定
+      if(sieve[i]) {
+        //   iずつ増えていくので倍数になる
+          for(let j = i*i; j <= n; j += i) {
+              sieve[j] = false;
+          }
+          
+      }
+  }
+  
+//   trueの数が素数の数になる
+  console.log(sieve.filter(num => num).length);
+
+</script>
+
+<template>
+    <div/>
+ </template>
