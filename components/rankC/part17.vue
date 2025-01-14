3eb31e2eeccaea67732ba8129a88ad626ff99687
yuki0829 <fledgling0829@gmail.com>
2025-01-14 14:29:34 +0900

    feat: 練習コード

---
+++ components/rankC/part17.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+    '50 3',
+    '40',
+    '90',
+    '10'
+];
+const [n, m] = lines[0].split(' ').map(Number);
+const w = lines.slice(1, lines.length).map(Number);
+
+const siwake = (w, n) => {
+    if(w <= n) {
+        console.log(n);
+    }else{
+        const lower = Math.floor(w / n) * n;
+        const upper = lower + n;
+        
+        console.log(Math.abs(lower - w) >= Math.abs(upper - w) ? upper : lower)
+    }
+}
+
+w.forEach(num => {
+    siwake(num, n);
+})
+</script>
+
+<template>
+    <div/>
+ </template>
---
+++ components/rankC/part18.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const lines = [
+    '3',
+    'mako 13 08/08 nara',
+    'megumi 14 11/02 saitama',
+    'taisei 16 12/04 nagano',
+    '14'
+];
+
+// クラスを活用
+const n = Number(lines[0]);
+const num = Number(lines[lines.length - 1]);
+
+class User {
+    constructor(name, old, birth, state) {
+        this.name = name;
+        this.old = old;
+        this.birth = birth;
+        this.state = state;
+    }
+}
+
+for(let i = 1; i <= n; i++) {
+    const [name, old, birth, state] = lines[i].split(' ');
+    const user = new User(name, old, birth, state);
+    
+    if(user.old == num) {
+        console.log(user.name);
+    }
+}
+
+// 配列を活用
+const n = Number(lines[0]);
+const num = Number(lines[lines.length - 1]);
+const a = [];
+
+for(let i = 1; i <= n; i++) {
+    const [name, old, birth, state] = lines[i].split(' ');
+    a.push({name, old, birth, state})
+}
+
+a.forEach(student => {
+    if(student.old == num) {
+        console.log(result.name);
+    }
+})
+</script>
+
+<template>
+    <div/>
+ </template>
