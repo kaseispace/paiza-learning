bb6c1f6fa8e2e8e3b120bc3f78d507ec9d7e2f6d
yuki0829 <fledgling0829@gmail.com>
2025-01-29 15:43:06 +0900

    feat: 練習コード

---
+++ components/rankB/practice/part16.vue
@@ -0,0 +1,13 @@
+<script setup>
+const [x, d_1, d_2, k] = lines[0].split(' ').map(Number);
+const dp = [x];
+
+for(let i = 1; i < k; i++) {
+    if((i+1) % 2 == 1) {
+        dp[i] = dp[i - 1] +  d_1;
+    }else{
+        dp[i] = dp[i - 1] +  d_2;
+    }
+}
+console.log(dp[dp.length - 1]);
+</script>
