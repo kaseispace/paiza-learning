07d707f3b3197a05bf77d83193dbb7c9f888055f
yuki0829 <fledgling0829@gmail.com>
2025-01-30 20:28:18 +0900

    feat: 練習コード

---
+++ components/rankB/practice/part17.vue
@@ -0,0 +1,20 @@
+<script setup>
+const [x, d_1, d_2] = lines[0].split(' ').map(Number);
+const q = Number(lines[1]);
+const dp = [x];
+const k = [];
+
+for(let i = 2; i < 2 + q; i++) {
+    k.push(Number(lines[i]));
+}
+
+for(let i = 1; i <= Math.max(...k); i++) {
+    if((i + 1) % 2 == 1) {
+        dp[i] = dp[i - 1] + d_1;
+    }else{
+        dp[i] = dp[i - 1] + d_2;
+    }
+}
+
+k.forEach(num => console.log(dp[num - 1]));
+</script>
