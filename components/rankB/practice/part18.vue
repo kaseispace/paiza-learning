4958d38abd7019ed262e6676ce8a6e8c731da9b5
yuki0829 <fledgling0829@gmail.com>
2025-02-01 20:57:00 +0900

    feat: 練習コード

---
+++ components/rankB/practice/part18.vue
@@ -0,0 +1,21 @@
+<script setup>
+const [n, a, b, c] = lines[0].split(' ').map(Number);
+const dp = Array(n + 1).fill(0);
+dp[0] = 1;
+
+for(let i = 1; i <= n; i++) {
+    if(i >= a) {
+        dp[i] += dp[i - a];
+    }
+    
+    if(i >= b) {
+        dp[i] += dp[i - b];
+    }
+    
+    if(i >= c) {
+        dp[i] += dp[i - c];
+    }
+}
+
+console.log(dp[n]);
+</script>
