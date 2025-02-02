4176e9cf88fca706e35483c80f4742c8b7166d71
yuki0829 <fledgling0829@gmail.com>
2025-02-02 20:36:32 +0900

    feat: 練習コード

---
+++ components/rankB/practice/part19.vue
@@ -0,0 +1,28 @@
+<script setup>
+// const n = 100;
+// const dp = Array(n + 1).fill(0);
+// dp[0] = 0;
+// dp[1] = 1;
+
+// for(let i = 2; i <= n; i++) {
+//     dp[i] = dp[i - 1] + dp[i - 2];
+// }
+
+// console.log(dp[n - 1]);
+
+// const test = (a) => {
+//     return function(b) {
+//         return a + b;
+//     }
+// }
+
+// const a = test(10);
+// console.log(a(3));
+// console.log(a(8));
+
+
+</script>
+
+<template>
+    <div/>
+ </template>
