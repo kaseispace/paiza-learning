a825d954d74a40dc1030a632e52e3a3b1a030749
yuki0829 <fledgling0829@gmail.com>
2025-02-05 11:42:42 +0900

    feat: 練習コード

---
+++ components/rankB/practice/part21.vue
@@ -0,0 +1,46 @@
+<script setup>
+// for(let i = 1; i <= 100; i++) {
+//     if(i % 15 === 0) {
+//         console.log('FizzBuzz');
+//     }else if(i % 3 === 0) {
+//         console.log('Fizz');
+//     }else if(i % 5 === 0) {
+//         console.log('Buzz');
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// const result = [];
+// for(let i = 2; i <= 100; i++) {
+//     let isPrime = true;
+
+//     for(let j = 2; j < i; j++) {
+//         if(i % j === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+
+//     if(isPrime) {
+//         result.push(i);
+//     }
+// }
+// console.log(result)
+
+// console.time('countSevensTime');
+// let count = 0;
+// for(let i = 0; i < 10000; i++) {
+//     const num = i.toString().split('');
+//     const result = num.filter(word => word == '7');
+//     count += result.length;
+// }
+// console.log(count);
+// console.timeEnd('countSevensTime');
+
+
+</script>
+
+<template>
+    <div/>
+ </template>
