5faf59ffc8cb629a2736a5f27fecd2f0ed01116b
yuki0829 <fledgling0829@gmail.com>
2025-02-06 13:56:56 +0900

    feat: 練習コード

---
+++ components/rankB/practice/part22.vue
@@ -0,0 +1,20 @@
+<script setup>
+const [n, x, a, y, b] = lines[0].split(' ').map(Number);
+const dp = Array(n + 1).fill(Infinity);
+dp[0] = 0;
+
+for(let i = 1; i <= n; i++) {
+    if(i >= y) {
+        dp[i] = Math.min(dp[i - x] + a, dp[i - y] + b);
+    }else if(i >= x) {
+        dp[i] = Math.min(dp[i - x] + a, b);
+    }else{
+        dp[i] = a;
+    }
+}
+console.log(dp[n]);
+</script>
+
+<template>
+    <div/>
+ </template>
