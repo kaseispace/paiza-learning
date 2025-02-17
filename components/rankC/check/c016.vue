f18ad4763ff1f05df7839db67266d7aa716afbdd
yuki0829 <fledgling0829@gmail.com>
2025-02-17 20:59:34 +0900

    feat: 練習コード

---
+++ components/rankC/check/c016.vue
@@ -0,0 +1,23 @@
+<script setup>
+const s = lines[0];
+const leetMapping = {
+    'A': '4',
+    'E': '3',
+    'G': '6',
+    'I': '1',
+    'O': '0',
+    'S': '5',
+    'Z': '2'
+};
+let result = '';
+
+for(const char of s) {
+    if(leetMapping[char]) {
+        result += leetMapping[char];
+    }else{
+        result += char;
+    }
+}
+
+console.log(result);
+</script>
