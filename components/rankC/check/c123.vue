e1bf1f4b3e96df27005c7f2667344ec61a2a725b
yuki0829 <fledgling0829@gmail.com>
2025-02-19 15:47:25 +0900

    feat: 練習コード

---
+++ components/rankC/check/c123.vue
@@ -0,0 +1,32 @@
+<script setup>
+const n = Number(lines[0]);
+const users = [];
+const m = Number(lines[n + 1]);
+
+class User {
+    constructor(age) {
+        this.age = Number(age);
+        this.count = 0;
+    }
+    
+    add(count) {
+        this.count = Math.min(this.age, this.count + count);
+    }
+}
+
+for(let i = 1; i <= n; i++) {
+    users.push(new User(lines[i]));
+}
+
+for(let i = n + 2; i <= n + m + 1; i++) {
+    const [a, b, c] = lines[i].split(' ').map(Number);
+    
+    for(let j = a; j <= b; j++) {
+        users[j - 1].add(c);
+    }
+}
+
+for(const user of users) {
+    console.log(user.count);
+}
+</script>
---
+++ components/rankC/check/c156.vue
@@ -0,0 +1,22 @@
+<script setup>
+const n = Number(lines[0]);
+let total_minutes = 0;
+let h = 0;
+let m = 0;
+
+for(let i = 1; i <= n; i++) {
+    const [e, l] = lines[i].split(' ');
+    const [e_h, e_m] = e.split(':').map(Number);
+    const [l_h, l_m] = l.split(':').map(Number);
+    
+    const e_total = e_h * 60 + e_m;
+    const l_total = l_h * 60 + l_m;
+    
+    total_minutes += l_total - e_total;
+}
+
+const total_h = Math.floor(total_minutes / 60);
+const total_m = total_minutes % 60;
+
+console.log(total_h, total_m);
+</script>
