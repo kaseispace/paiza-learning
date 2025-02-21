5ad55c41acf91ee42fc662702042d1abd2a5b0bf
yuki0829 <fledgling0829@gmail.com>
2025-02-21 21:08:21 +0900

    feat: 練習コード

---
+++ components/rankC/check/075.vue
@@ -0,0 +1,17 @@
+<script setup>
+let [n, m] = lines[0].split(' ').map(Number);
+let point = 0;
+
+for(let i = 1; i <= m; i++) {
+    const s = Number(lines[i]);
+    
+    if(point >= s) {
+        point -= s;
+    }else{
+        n -= s;
+        point += Math.floor(s * 0.1);
+    }
+    
+    console.log(n, point);
+}
+</script>
