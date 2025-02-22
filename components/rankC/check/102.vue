03012b4fc8907528546d501e341e360074a73a9c
yuki0829 <fledgling0829@gmail.com>
2025-02-22 21:59:26 +0900

    feat: 練習コード

---
+++ components/rankC/check/102.vue
@@ -0,0 +1,21 @@
+<script setup>
+const m = Number(lines[0]);
+const n = Number(lines[m + 1]);
+const A = new Set(lines.slice(1, m + 1).map(Number));
+const B = new Set(lines.slice(m + 2).map(Number));
+let check = true;
+
+for(let i = 1; i <= 31; i++) {
+    if(A.has(i) && B.has(i)) {
+        console.log(check ? 'A' : 'B');
+        check = !check;
+    } else if(A.has(i)) {
+        console.log('A');
+        
+    } else if(B.has(i)) {
+        console.log('B');
+    } else {
+        console.log('x');
+    }
+}
+</script>
