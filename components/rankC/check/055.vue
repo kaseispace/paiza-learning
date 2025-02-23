fc0bb6e547b34a377e5944b23280085de5d188a7
yuki0829 <fledgling0829@gmail.com>
2025-02-23 21:11:29 +0900

    feat: 練習コード

---
+++ components/rankC/check/055.vue
@@ -0,0 +1,17 @@
+<script setup>
+const n = Number(lines[0]);
+const keyword = lines[1];
+let foundLogs = [];
+
+for(let i = 2; i <= n + 1; i++) {
+    if(lines[i].includes(keyword)) {
+        foundLogs.push(lines[i]);
+    }
+}
+
+if(foundLogs.length > 0) {
+    foundLogs.forEach(log => console.log(log));
+}else{
+    console.log('None');
+}
+</script>
