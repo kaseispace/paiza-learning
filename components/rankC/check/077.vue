7816a74690ae422b097cbbcc3fe637635bebb0d2
yuki0829 <fledgling0829@gmail.com>
2025-02-27 14:52:08 +0900

    feat: 練習コード

---
+++ components/rankC/check/077.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const lines = [
+    '3 25',
+    '-2 17',
+    '-7 20',
+    '2 24'
+]
+const [k, n] = lines[0].split(' ').map(Number);
+
+for(let i = 1; i <= k; i++) {
+    const [d, a] = lines[i].split(' ').map(Number);
+    
+    if(d >= 10) {
+        console.log('D');
+    }else{
+        const score = d >= 1 ? Math.floor(a / n * 80) : a / n * 100;
+        
+        switch (true) {
+            case score >= 80:
+                console.log('A');
+                break;
+            case score >= 70:
+                console.log('B');
+                break;
+            case score >= 60:
+                console.log('C');
+                break;
+            default:
+                console.log('D');
+        }
+    }
+}
+</script>
---
+++ components/rankC/check/c103.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+    '6 2',
+    '2 foo',
+    '3 bar'
+]
+const [n, m] = lines[0].split(' ').map(Number);
+
+for(let i = 1; i <= n; i++) {
+    const result = [];
+    
+    for(let j = 1; j <= m; j++) {
+        const [a, b] = lines[j].split(' ');
+        if(i % Number(a) === 0) {
+            result.push(b);
+        }
+    }
+    
+    console.log(result.length > 0 ? result.join(' ') : i);
+}
+</script>
