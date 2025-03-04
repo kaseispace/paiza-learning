7349f02a7f3e149001ac5e5689ed29363f514b5c
yuki0829 <fledgling0829@gmail.com>
2025-03-04 21:22:52 +0900

    feat: 練習コード

---
+++ components/rankC/check/c111.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const lines = [
+    '8 3',
+    '10100101'
+]
+const [N, M] = lines[0].split(' ').map(Number);
+const s = lines[1];
+const S = [];
+let result = '0'.repeat(M).split('').map(Number);
+
+for(let i = 0; i < N; i += M) {
+    let block = s.slice(i, i + M);
+    if(block.length < M) {
+        block = block.padEnd(M, '0');
+    }
+    
+    S.push(block.split('').map(Number));
+}
+
+S.forEach(block => {
+    for(let i = 0; i < M; i++) {
+        result[i] ^= block[i];
+    }
+});
+
+console.log(result.join(''));
+</script>
---
+++ components/rankC/check/c132.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+    '5',
+    '3'
+]
+const n = Number(lines[0]);
+const m = Number(lines[1]);
+
+const gcd = (a: number, b: number) => {
+    if (b === 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+};
+
+const canCleanAllRooms = (N: number, M: number) => {
+    return gcd(N, M) === 1 ? 'yes' : 'no';
+};
+
+console.log(canCleanAllRooms(n, m));
+</script>
