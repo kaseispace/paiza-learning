77783910e0f04cf51f7ee7169aba32cea7b9991d
yuki0829 <fledgling0829@gmail.com>
2025-03-05 15:40:51 +0900

    feat: 練習コード

---
+++ components/rankC/check/c118.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const lines = [
+    '8 3 6',
+    '3',
+    '6',
+    '4'
+]
+const [D, N, L] = lines[0].split(' ').map(Number);
+const d = Array.from({length: D}, (_, i) => i);
+let currentPosition = 0;
+let totalMoves = 0;
+let result = 'Yes';
+
+for(let i = 1; i <= N; i++) {
+    const target = Number(lines[i]);
+    
+    const clockwise = (target - currentPosition + D) % D;
+    const counterClockwise = (currentPosition - target + D) % D;
+    
+    totalMoves += Math.min(clockwise, counterClockwise);
+    currentPosition = target;
+    
+    if(totalMoves > L) {
+        result = 'No';
+        break;
+    }
+}
+
+console.log(result);
+</script>
---
+++ components/rankC/check/c134.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = [
+    '26'
+]
+let n = Number(lines[0]);
+const alfa = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+n -= 1;
+let name = '';
+
+while(n >= 0) {
+    const remainder = n % 26;
+    name = alfa[remainder] + name;
+    n = Math.floor(n / 26) - 1;
+}
+
+console.log(name);
+</script>
