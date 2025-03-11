131ac86f5155cff9136c1ee24ffd64001f4d04f7
yuki0829 <fledgling0829@gmail.com>
2025-03-11 20:52:48 +0900

    feat: 練習コード

---
+++ components/standardAlgorithm/cumulativeSum/1.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = [
+    '10 3 8',
+    'bwwbwbbwbw'
+];
+const [n, x, y] = lines[0].split(' ').map(Number);
+const a = lines[1];
+const s = [0];
+
+for(let i = 0; i < n; i++) {
+    const w = a[i] === 'b' ? 1 : 0;
+    s.push(s[i] + w);
+}
+
+console.log(s[y] - s[x - 1]);
+</script>
