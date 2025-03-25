44f3612ed4a288ba5f92c88cf0282d65521cf0ff
yuki0829 <fledgling0829@gmail.com>
2025-03-25 11:39:58 +0900

    feat: 練習コード

---
+++ components/review/1.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const lines = [
+    '10 15',
+    '1 5 9 1 20 5 3 6 5 4'
+];
+const [N, K] = lines[0].split(' ').map(Number);
+const a = lines[1].split(' ').map(Number);
+
+let currentSum = 0;
+let left = 0;
+let maxLength = 0;
+
+for(let right = 0; right < a.length; right++) {
+    currentSum += a[right];
+    
+    while (currentSum > K && left <= right) {
+        currentSum -= a[left];
+        left++;
+    }
+    
+    maxLength = Math.max(maxLength, right - left + 1);
+}
+
+console.log(maxLength);
+</script>
---
+++ components/review/2.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = ['1 2 3 4 5 6 7 8 9 10'];
+const a = lines[0].split(' ').map(Number);
+const s = [0];
+let maxValue = 0;
+
+for(let i = 0; i < 10; i++) {
+    s.push(a[i] + s[i]);
+}
+
+for(let i = 0; i <= 7; i++) {
+    const num = s[i + 3] - s[i];
+    maxValue = Math.max(maxValue, num);
+}
+
+console.log(maxValue);
+</script>
