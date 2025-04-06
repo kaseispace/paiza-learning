92fa2a731b9824453d4ee9c173a8c7326bb1f1fc
yuki0829 <fledgling0829@gmail.com>
2025-04-06 22:06:07 +0900

    feat: 練習コード

---
+++ components/review/3.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const N = 57;
+const isPrime = (n: number) => {
+    if(n <= 1) return false;
+    if(n === 2) return true;
+    if(n % 2 === 0) return false;
+    
+    for(let i = 3; i * i <= n; i += 2) {
+        if(n % i === 0) return false;
+    }
+    
+    return true;
+};
+
+console.log(isPrime(N) ? 'YES' : 'NO');
+</script>
