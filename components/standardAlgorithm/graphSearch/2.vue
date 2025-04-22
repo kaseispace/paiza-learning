59bc4be33c790766c91e3f825599e1d56ccdf3a9
yuki0829 <fledgling0829@gmail.com>
2025-04-22 15:04:02 +0900

    feat: 練習コード

---
+++ components/standardAlgorithm/graphSearch/2.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const lines = [
+    "6 2", 
+    "2 0"
+];
+const [H, W] = lines[0].split(' ').map(Number);
+const [startY, startX] = lines[1].split(' ').map(Number);
+const grid: string[][] = Array.from({length: H}, () => Array(W).fill('.'));
+const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+const queue = [[startY, startX, 0]]; // BFS用の探索するべきマスを管理するリスト（キュー）
+
+grid[startY][startX] = '*';
+while(queue.length > 0) {
+    const next = queue.shift();
+    if(!next) break;
+
+    const [y, x, depth] = next;
+    if(depth === 3) break; // breakだとdepthが2のものまだ存在した場合スルーされてしまうので
+    
+    for(const [dy, dx] of directions) {
+        const ny = dy + y;
+        const nx = dx + x;
+        
+        if(ny >= 0 && ny < H && nx >= 0 && nx < W && grid[ny][nx] === '.') {
+            grid[ny][nx] = '*';
+            queue.push([ny, nx, depth + 1]);
+        }
+    }
+}
+
+grid.forEach(row => console.log(row.join('')));
+</script>
