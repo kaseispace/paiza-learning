004d556273515a1c78cb509cb3893f8e61635db0
yuki0829 <fledgling0829@gmail.com>
2025-05-27 20:55:46 +0900

    feat: 練習コード

---
+++ components/rankB/check/081.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const lines = [
+  '4 4',
+  '..#.',
+  '..#.',
+  '###.',
+  '..#.',
+]
+const [H, W] = lines[0].split(' ').map(Number)
+const grid = lines.slice(1).map(line => line.split(''))
+let ropeCount = 0
+
+const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+for (let y = 0; y < H; y++) {
+  for (let x = 0; x < W; x++) {
+    if (grid[y][x] === '#') {
+      for (const [dy, dx] of directions) {
+        const ny = dy + y
+        const nx = dx + x
+
+        if (ny < 0 || ny >= H || nx < 0 || nx >= W || grid[ny][nx] === '.') {
+          ropeCount++
+        }
+      }
+    }
+  }
+}
+
+console.log(ropeCount)
+</script>
