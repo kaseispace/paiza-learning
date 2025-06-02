5b2097bab349066f314fb75af42c3f9e7b204135
yuki0829 <fledgling0829@gmail.com>
2025-06-02 10:24:21 +0900

    feat: 練習コード

---
+++ components/rankB/check/001-050/b040.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = [
+  '1 qwertyuiopasdfghjklzxcvbnm',
+  'hqomq gfsoft iqeaqzigf',
+]
+const [n, T] = lines[0].split(' ')
+const S = lines[1]
+
+const originalAlphabet = 'abcdefghijklmnopqrstuvwxyz'
+const transformMap: Map<string, string> = new Map()
+
+for (let i = 0; i < 26; i++) {
+  transformMap.set(T[i], originalAlphabet[i])
+}
+
+let decrypted = S
+for (let i = 0; i < Number(n); i++) {
+  decrypted = decrypted.split('').map(char => transformMap.get(char) || char).join((''))
+}
+
+console.log(decrypted)
+</script>
