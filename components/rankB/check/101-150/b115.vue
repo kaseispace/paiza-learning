c73d6dfa88a56a12093876b9ad5b46c943a2c194
yuki0829 <fledgling0829@gmail.com>
2025-06-13 15:30:51 +0900

    feat: 練習コード

---
+++ components/rankB/check/101-150/b115.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const lines = [
+  '6',
+  'coin',
+  'come',
+  'creation',
+  'economic',
+  'rate',
+  'reaction',
+]
+const N = Number(lines[0])
+const words = lines.slice(1)
+const sortedWords: Map<string, number> = new Map()
+let result = 0
+
+words.forEach((word) => {
+  const sortWord = word.split('').sort().join('')
+  sortedWords.set(sortWord, (sortedWords.get(sortWord) ?? 0) + 1)
+})
+
+for (let i = 0; i < N; i++) {
+  for (let j = i; j < N; j++) {
+    const merged = [...words[i], ...words[j]].sort().join('')
+    if (sortedWords.has(merged)) {
+      result += sortedWords.get(merged)!
+    }
+  }
+}
+
+console.log(result)
+</script>
