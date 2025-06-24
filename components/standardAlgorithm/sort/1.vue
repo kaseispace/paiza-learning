5f1ce18076131d24940e2e0179f18471f38757b4
yuki0829 <fledgling0829@gmail.com>
2025-06-24 15:22:10 +0900

    feat: 練習コード

---
+++ components/standardAlgorithm/sort/1.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '4 1 3 5 2',
+]
+const N = Number(lines[0])
+const A = lines[1].split(' ').map(Number)
+
+// 昇順にソートするバブルソート
+for (let i = 0; i < N - 1; i++) {
+  for (let j = N - 1; j > i; j--) {
+    if (A[j - 1] > A[j]) {
+      [A[j - 1], A[j]] = [A[j], A[j - 1]]
+    }
+  }
+
+  console.log(A.join(' '))
+}
+</script>
