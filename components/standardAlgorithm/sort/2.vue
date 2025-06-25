68f679b00622e7c3ac4aa6fe8acd28f1fdb29bf2
yuki0829 <fledgling0829@gmail.com>
2025-06-25 22:04:12 +0900

    feat: 練習コード

---
+++ components/standardAlgorithm/sort/2.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '4 1 3 5 2',
+]
+const N = Number(lines[0])
+const A = lines[1].split(' ').map(Number)
+
+// 昇順にソートする選択ソート
+for (let i = 0; i < N - 1; i++) {
+  let index = i
+
+  for (let j = i + 1; j < N; j++) {
+    if (A[index] > A[j]) {
+      index = j
+    }
+  }
+
+  [A[i], A[index]] = [A[index], A[i]]
+  console.log(A.join(' '))
+}
+</script>
