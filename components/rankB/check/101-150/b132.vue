db96ccb24894b1be60b5315aa3ca0d73a08655ac
yuki0829 <fledgling0829@gmail.com>
2025-06-29 14:57:21 +0900

    feat; 練習コード

---
+++ components/rankB/check/101-150/b132.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const lines = [
+  '3 5',
+  '10',
+  '20',
+  '23',
+]
+const [N, c] = lines[0].split(' ').map(Number)
+const times = lines.slice(1).map(Number)
+let max = 0
+let currentStart = times[0] - c
+let currentEnd = times[0]
+
+for (let i = 1; i < N; i++) {
+  const start = times[i] - c
+  const end = times[i]
+
+  if (currentEnd >= start) {
+    currentEnd = Math.max(currentEnd, end)
+  }
+  else {
+    max = Math.max(max, currentEnd - currentStart)
+    currentStart = start
+    currentEnd = end
+  }
+}
+
+max = Math.max(max, currentEnd - currentStart)
+console.log(max)
+</script>
