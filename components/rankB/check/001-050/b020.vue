ec18eaa6fe30644f653c1019afbbecbf7cf8a26b
yuki0829 <fledgling0829@gmail.com>
2025-07-06 11:31:06 +0900

    feat: 練習コード

---
+++ components/rankB/check/001-050/b020.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  'go to blank page',
+  'go to bja n',
+  'go to va',
+  'use the back button',
+  'use the back button',
+]
+const actions = lines.slice(2).map(line => line.split(' '))
+const pages = ['blank page']
+
+console.log('blank page')
+
+actions.forEach((page) => {
+  if (page[0] === 'go') {
+    const p = page.slice(2).join(' ')
+    pages.push(p)
+    console.log(p)
+  }
+  else {
+    pages.pop()
+    console.log(pages[pages.length - 1])
+  }
+})
+</script>
