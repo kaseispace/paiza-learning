9ca376fe32c0e694efab0c22568d6b81088253fe
yuki0829 <fledgling0829@gmail.com>
2025-07-14 15:48:10 +0900

    feat: 練習コード

---
+++ components/rankB/check/051-100/b065.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const lines = ['++-+--+++- +--+']
+const [S, T] = lines[0].split(' ')
+const seen: Set<string> = new Set()
+let current = S
+
+const getNext = (state: string) => {
+  let next = ''
+  for (let i = 0; i < 10; i++) {
+    const left = state[(i + 9) % 10]
+    const right = state[(i + 1) % 10]
+    const idx = (left === '+' ? 2 : 0) + (right === '+' ? 1 : 0)
+    next += T[idx]
+  }
+  return next
+}
+
+while (true) {
+  const next = getNext(current)
+  if (next === S) {
+    console.log('YES')
+    break
+  }
+
+  if (seen.has(next)) {
+    console.log('NO')
+    break
+  }
+
+  seen.add(next)
+  current = next
+}
+</script>
