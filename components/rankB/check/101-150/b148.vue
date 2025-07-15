68619fc1eae072b7f94f66577796205bca399ba4
yuki0829 <fledgling0829@gmail.com>
2025-07-15 14:18:43 +0900

    feat: 練習コード

---
+++ components/rankB/check/101-150/b148.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '12',
+  '5',
+  '36',
+  '12',
+]
+const users = lines.slice(1).map(Number)
+const Users = lines.slice(1).map(Number)
+const ranking: Map<number, number> = new Map()
+let count = 1
+
+users.sort((a, b) => b - a)
+users.forEach((user) => {
+  if (!ranking.has(user)) {
+    ranking.set(user, count)
+  }
+
+  count++
+})
+
+Users.forEach((user) => {
+  console.log(ranking.get(user))
+})
+</script>
