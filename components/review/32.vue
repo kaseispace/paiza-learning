10d812f1c7d3271cfb2990984269441de5f5f8d5
yuki0829 <fledgling0829@gmail.com>
2025-07-28 22:31:29 +0900

    feat: 練習コード

---
+++ components/review/32.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const lines = [
+  '...X.',
+  '...X.',
+  '...X.',
+  '...X.',
+  'OOOO.',
+]
+let left = ''
+let right = ''
+let result = 'D'
+
+for (let i = 0; i < 5; i++) {
+  let col = ''
+  let row = ''
+
+  for (let j = 0; j < 5; j++) {
+    col += lines[i][j]
+    row += lines[j][i]
+  }
+
+  left += lines[i][i]
+  right += lines[i][4 - i]
+
+  if (col === 'OOOOO' || row === 'OOOOO' || left === 'OOOOO' || right === 'OOOOO') {
+    result = 'O'
+    break
+  }
+  else if (col === 'XXXXX' || row === 'XXXXX' || left === 'XXXXX' || right === 'XXXXX') {
+    result = 'X'
+    break
+  }
+}
+
+console.log(result)
+</script>
