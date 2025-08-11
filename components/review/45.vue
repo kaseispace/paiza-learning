6ce3f307ef110502e108f18ac8a89e2b4f3f6167
yuki0829 <fledgling0829@gmail.com>
2025-08-11 22:37:37 +0900

    feat: 練習コード

---
+++ components/review/45.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const lines = [
+  '7',
+  '1 2 + 3 4 + -',
+]
+const tokens = lines[1].split(' ')
+
+const stack: number[] = []
+
+for (const token of tokens) {
+  if (token === '+' || token === '-') {
+    const b = stack.pop()!
+    const a = stack.pop()!
+    const result = token === '+' ? a + b : a - b
+    stack.push(result)
+  }
+  else {
+    stack.push(Number(token))
+  }
+}
+
+console.log(stack[0])
+</script>
