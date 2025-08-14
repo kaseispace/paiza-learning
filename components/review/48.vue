6bd7421bf258580254e188aecc9dcd1cb2088a74
yuki0829 <fledgling0829@gmail.com>
2025-08-14 20:44:56 +0900

    feat: 練習コード

---
+++ components/review/48.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+  '3 3',
+  '1 1',
+]
+const [H, W] = lines[0].split(' ').map(Number)
+const [y, x] = lines[1].split(' ').map(Number)
+const grid: string[][] = Array.from({ length: H }, () => Array(W).fill('.'))
+const directions = [[0, 0], [1, 0], [-1, 0], [0, 1], [0, -1]]
+
+directions.forEach(([dy, dx]) => {
+  const ny = dy + y
+  const nx = dx + x
+
+  if (ny >= 0 && ny < H && nx >= 0 && nx < W) {
+    grid[dy + y][dx + x] = '*'
+  }
+})
+
+grid.forEach(row => console.log(row.join('')))
+</script>
