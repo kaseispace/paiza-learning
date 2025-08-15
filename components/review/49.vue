6ff1b295abb3d0cf6004ff0d9dad46de11e1a4d0
yuki0829 <fledgling0829@gmail.com>
2025-08-15 20:50:25 +0900

    feat: 練習コード

---
+++ components/review/49.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const lines = [
+  '2 10',
+  '1 3 5',
+  '2 5 4',
+]
+const [Q, M] = lines[0].split(' ').map(Number)
+const diff: number[] = Array(1001).fill(0)
+
+for (let i = 1; i <= Q; i++) {
+  const [S, T, P] = lines[i].split(' ').map(Number)
+  diff[S] += P
+  diff[T] -= P
+}
+
+let current = 0
+for (let t = 0; t <= 1000; t++) {
+  current += diff[t]
+  if (current >= M) {
+    break
+  }
+}
+
+console.log(current >= M ? 'No' : 'Yes')
+</script>
