50ef433ff957f1355813918111126962001c33a7
yuki0829 <fledgling0829@gmail.com>
2025-08-20 16:40:02 +0900

    feat: 練習コード

---
+++ components/review/6/54.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const S = 'DDRRAAAAALLLRB'
+
+const commands = {
+  LLLRB: 'rolling',
+  DDRRA: 'upper',
+  AAAAA: 'rush',
+}
+
+let i = 0
+while (i < S.length) {
+  let matched = false
+  for (const [cmd, name] of Object.entries(commands)) {
+    if (S.startsWith(cmd, i)) {
+      console.log(name)
+      i += cmd.length // コマンド分を消費
+      matched = true
+      break
+    }
+  }
+  if (!matched) {
+    i++
+  }
+}
+</script>
