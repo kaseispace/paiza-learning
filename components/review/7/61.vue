c3d80b4fb5f8a8c5b8461878a8e3a87b8db3a4c8
yuki0829 <fledgling0829@gmail.com>
2025-08-27 16:34:33 +0900

    feat: 練習コード

---
+++ components/review/7/61.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  'abc',
+  'bca',
+  'cab',
+  'aaa',
+  'abc',
+]
+const strings = lines.slice(1)
+
+const hash = (s: string) => {
+  let h = 0
+  const prime = 31
+  for (let i = 0; i < s.length; i++) {
+    h = (h * prime + s.charCodeAt(i)) % 1000000007
+  }
+  return h % 100
+}
+
+for (const s of strings) {
+  console.log(hash(s))
+}
+</script>
