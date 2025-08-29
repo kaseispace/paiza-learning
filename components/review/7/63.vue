13570970a7857e6db7409f167d5b1b1b37ed868f
yuki0829 <fledgling0829@gmail.com>
2025-08-29 22:00:49 +0900

    feat: 練習コード

---
+++ components/review/7/63.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const lines = ['12345 5']
+const [N, A] = lines[0].split(' ').map(Number)
+
+const gcd = (a: number, b: number) => {
+  while (b !== 0) {
+    [a, b] = [b, a % b]
+  }
+  return a
+}
+
+const result: number[] = []
+for (let B = 1; B <= 1000; B++) {
+  if (B === A) continue
+  const g = gcd(A, B)
+  if (N % g !== 0) {
+    result.push(B)
+  }
+}
+
+if (result.length === 0) {
+  console.log(-1)
+}
+else {
+  console.log(result.join('\n'))
+}
+</script>
