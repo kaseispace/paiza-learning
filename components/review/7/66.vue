e7a15f8eeff4d8a25b00affdc7922f4003cbe1af
yuki0829 <fledgling0829@gmail.com>
2025-09-01 16:57:55 +0900

    feat: 練習コード

---
+++ components/review/7/66.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const lines = [
+  '1 4',
+  '4',
+  '2 1',
+  '1 1 4',
+  '1 1 2',
+  '2 1',
+]
+const [N] = lines[0].split(' ').map(Number)
+const A = lines.slice(1, 1 + N).map(Number)
+const queries = lines.slice(1 + N)
+const list = [...A]
+
+for (const query of queries) {
+  const parts = query.split(' ').map(Number)
+  if (parts[0] === 1) {
+    const [_, P, X] = parts
+    list.splice(P - 1, 0, X)
+  }
+  else if (parts[0] === 2) {
+    const [_, P] = parts
+    list.splice(P - 1, 1)
+  }
+}
+
+for (const val of list) {
+  console.log(val)
+}
+</script>
