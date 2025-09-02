c69812d367d46bf62b753abb34abab79bd7482cb
yuki0829 <fledgling0829@gmail.com>
2025-09-02 14:55:24 +0900

    feat: 練習コード

---
+++ components/review/7/67.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const lines = [
+  '26',
+  '6',
+  '6',
+  '10',
+  '9',
+]
+const K = Number(lines[0])
+const A = lines.slice(1).map(Number)
+let found = false
+
+for (let bit = 0; bit < (1 << 4); bit++) {
+  let sum = 0
+  for (let i = 0; i < 4; i++) {
+    if (bit & (1 << i)) {
+      sum += A[i]
+    }
+  }
+  if (sum === K) {
+    found = true
+    break
+  }
+}
+
+console.log(found ? 'Yes' : 'No')
+</script>
