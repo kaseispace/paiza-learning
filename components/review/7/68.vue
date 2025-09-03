c997726fc3af6abff631a064d126c70ba0c443ce
yuki0829 <fledgling0829@gmail.com>
2025-09-03 15:43:59 +0900

    feat: 練習コード

---
+++ components/review/7/68.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const lines = [
+  '3 1 1',
+  '9',
+  '10',
+  '6',
+]
+const [N, L, R] = lines[0].split(' ').map(Number)
+const A = lines.slice(1, 1 + N).map(Number)
+
+if (L < R) {
+  A.splice(L - 1, R - L)
+}
+
+for (const val of A) {
+  console.log(val)
+}
+</script>
