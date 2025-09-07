8081790a54efa74c2e5fda520dd50bdc602ba2d3
yuki0829 <fledgling0829@gmail.com>
2025-09-07 15:23:19 +0900

    feat: 練習コード

---
+++ components/review/8/72.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = ['2 5']
+const MOD = 10000000
+const [xStr, kStr] = lines[0].split(' ').map(Number)
+const x = Number(xStr)
+const k = Number(kStr)
+
+let cur = x % MOD
+const out: string[] = []
+for (let i = 0; i < k; i++) {
+  out.push(String(cur))
+  cur = (cur * cur) % MOD
+}
+
+console.log(out.join('\n'))
+</script>
