91f0ada9acc7bbbaf96c71fe7accddc981924768
yuki0829 <fledgling0829@gmail.com>
2025-09-11 21:16:57 +0900

    feat: 練習コード

---
+++ components/review/8/76.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '3 6 9 12 15',
+]
+const n = Number(lines[0])
+const a = lines[1].split(' ').map(Number)
+
+const diff = a[1] - a[0]
+let diffResult = true
+for (let i = 1; i < n; i++) {
+  const d = a[i] - a[i - 1]
+  if (d !== diff) {
+    diffResult = false
+    break
+  }
+}
+
+let ratioResult = true
+for (let i = 2; i < n; i++) {
+  if (a[i] * a[i - 2] !== a[i - 1] * a[i - 1]) {
+    ratioResult = false
+    break
+  }
+}
+
+console.log(diffResult ? 'Yes' : 'No')
+console.log(ratioResult ? 'Yes' : 'No')
+</script>
