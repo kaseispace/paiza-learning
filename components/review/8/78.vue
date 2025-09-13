3108b80486b94d8992ead087baacd6c8cb6507cd
yuki0829 <fledgling0829@gmail.com>
2025-09-13 20:58:29 +0900

    feat: 練習コード

---
+++ components/review/8/78.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const lines = ['1 3 10']
+const [N, X, K] = lines[0].split(' ').map(Number)
+
+const countCL = (T: number) => {
+  const q = Math.floor(T / 4)
+  const r = T % 4
+  return 2 * q + Math.max(0, r - 2)
+}
+
+const extra = X * (countCL(K) - countCL(4 * N))
+console.log(extra.toString())
+</script>
