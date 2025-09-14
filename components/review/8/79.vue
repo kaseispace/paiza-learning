592d7d0407011dad8e89c400842c4bb8b8b70a22
yuki0829 <fledgling0829@gmail.com>
2025-09-14 20:52:46 +0900

    feat: 練習コード

---
+++ components/review/8/79.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const lines = [
+  '2 4',
+  'paiza zero room omega',
+]
+const [N, M] = lines[0].split(' ').map(Number)
+const words = lines[1].split(' ')
+
+let isValid = true
+for (let i = 0; i < M - 1; i++) {
+  const tail = words[i].slice(-N)
+  const head = words[i + 1].slice(0, N)
+  if (tail !== head) {
+    isValid = false
+    break
+  }
+}
+
+console.log(isValid ? 'YES' : 'NO')
+</script>
