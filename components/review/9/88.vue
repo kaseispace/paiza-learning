4f21f4e3e8ec31fa59c54ac5ae61482185c24563
yuki0829 <fledgling0829@gmail.com>
2025-09-23 21:11:05 +0900

    feat: 練習コード

---
+++ components/review/9/88.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const lines = [
+  '14',
+  '((((())())()))',
+]
+const S = lines[1]
+let count = 0
+let isValid = true
+
+for (const c of S) {
+  if (c === '(') count++
+  else if (c === ')') count--
+
+  if (count < 0) {
+    isValid = false
+    break
+  }
+}
+
+if (count !== 0) isValid = false
+
+console.log(isValid ? 'Yes' : 'No')
+</script>
