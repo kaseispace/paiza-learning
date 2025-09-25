345e12c713c4c9be49acfd538a3cedafcab2a380
yuki0829 <fledgling0829@gmail.com>
2025-09-25 21:13:00 +0900

    feat: 練習コード

---
+++ components/review/9/90.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '6',
+  '7',
+  '10',
+  '1',
+  '15',
+]
+const T = parseInt(lines[0])
+
+for (let i = 1; i <= T; i++) {
+  const x = BigInt(lines[i])
+  console.log(x.toString(2))
+}
+</script>
