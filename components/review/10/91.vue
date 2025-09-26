373061e1869f2fd7bb2060a5916f4950863f86ea
yuki0829 <fledgling0829@gmail.com>
2025-09-26 20:44:04 +0900

    feat: 練習コード

---
+++ components/review/10/91.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const lines = [
+  '3 1',
+  '9',
+  '4',
+  '4',
+]
+const [N, P] = lines[0].split(' ').map(Number)
+const A = lines.slice(1, 1 + N).map(Number)
+
+A.splice(P - 1, 1)
+
+A.forEach(x => console.log(x))
+</script>
