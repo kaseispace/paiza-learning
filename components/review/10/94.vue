9b638c545c8ee8c6f054bdd695ea44f6077aa92a
yuki0829 <fledgling0829@gmail.com>
2025-09-29 20:24:41 +0900

    feat: 練習コード

---
+++ components/review/10/94.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const lines = [
+  '9',
+  '1 2 1 3 1 4 1 5 1',
+]
+const N = Number(lines[0])
+const A = lines[1].split(' ').map(Number)
+
+const dp: number[] = Array(N).fill(1)
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < i; j++) {
+    if (A[j] < A[i]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1)
+    }
+  }
+}
+
+console.log(Math.max(...dp))
+</script>
