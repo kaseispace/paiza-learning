9e03ddd8e4c395d9e60f38bbcd3c4f0a14a59349
yuki0829 <fledgling0829@gmail.com>
2025-10-04 20:22:22 +0900

    feat: 練習コード

---
+++ components/review/10/99.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '1 10 100 1000',
+  '3',
+  '1 10',
+  '1 1000',
+  '5 105',
+]
+const prices = lines[1].split(' ').map(Number)
+const queries = lines.slice(3).map(line => line.split(' ').map(Number))
+
+for (const [l, r] of queries) {
+  const count = prices.filter(p => l <= p && p <= r).length
+  console.log(count)
+}
+</script>
