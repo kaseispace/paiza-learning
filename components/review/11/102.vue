b1d7938fed2548ac2b82b28ea628954df392eea7
yuki0829 <fledgling0829@gmail.com>
2025-10-07 20:28:55 +0900

    feat: 練習コード

---
+++ components/review/11/102.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const lines = [
+  '4 19',
+  '6',
+  '10',
+  '2',
+  '5',
+]
+const [, K] = lines[0].split(' ').map(Number)
+const A = lines.slice(1).map(Number)
+
+const dp: boolean[] = Array(K + 1).fill(false)
+dp[0] = true
+
+for (const a of A) {
+  for (let i = K; i >= a; i--) {
+    if (dp[i - a]) {
+      dp[i] = true
+    }
+  }
+}
+
+console.log(dp[K] ? 'Yes' : 'No')
+</script>
