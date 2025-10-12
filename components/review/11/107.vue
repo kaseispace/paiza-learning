ad599f13c72f0f097fb4889f46a0b8e4d29e4194
yuki0829 <fledgling0829@gmail.com>
2025-10-12 10:36:16 +0900

    feat: 練習コード

---
+++ components/review/11/107.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const lines = ['45 15']
+const [A, B] = lines[0].split(' ').map(Number)
+
+const gcd = (a: number, b: number) => {
+  while (b !== 0) {
+    [a, b] = [b, a % b]
+  }
+  return a
+}
+
+console.log(gcd(A, B))
+</script>
