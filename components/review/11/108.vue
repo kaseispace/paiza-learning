d06ccd286994bb64928144e99d9153d1a7a6686e
yuki0829 <fledgling0829@gmail.com>
2025-10-13 11:23:23 +0900

    feat: 練習コード

---
+++ components/review/11/108.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const lines = [
+  '3',
+  '2',
+  '4',
+  '8',
+]
+const A = lines.slice(1).map(Number)
+
+const gcd = (a: number, b: number) => {
+  while (b !== 0) {
+    [a, b] = [b, a % b]
+  }
+  return a
+}
+
+const lcm = (a: number, b: number) => {
+  return (a * b) / gcd(a, b)
+}
+
+const totalGCD = A.reduce((acc, val) => gcd(acc, val))
+const totalLCM = A.reduce((acc, val) => lcm(acc, val))
+
+console.log(totalGCD)
+console.log(totalLCM)
+</script>
