8a769d7b0efd184fc9568976b9437284ad9b0bba
yuki0829 <fledgling0829@gmail.com>
2025-10-14 20:15:41 +0900

    feat: 練習コード

---
+++ components/review/11/109.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const lines = [
+  '3',
+  '3',
+  '5',
+  '9',
+]
+const C = lines.slice(1).map(Number)
+
+const gcd = (a: number, b: number) => {
+  while (b !== 0) [a, b] = [b, a % b]
+  return a
+}
+
+const lcm = (a: number, b: number) => {
+  return (a * b) / gcd(a, b)
+}
+
+const widths = C.map(c => c - 1)
+const result = widths.reduce((acc, val) => lcm(acc, val))
+
+console.log(result)
+</script>
