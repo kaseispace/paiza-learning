5f1d95fd81a16a4f233f5ebcedd65d3f3b2686b0
yuki0829 <fledgling0829@gmail.com>
2025-10-16 21:09:59 +0900

    feat: 練習コード

---
+++ components/review/12/111.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = [
+  '\\/___',
+  '/_/__',
+  '\\/___',
+  '/\\__\\',
+  '\\/\\_\\',
+]
+const [H] = lines[0].split(' ').map(Number)
+const map: Record<string, number> = { '_': 0, '/': 1, '\\': 2 }
+
+for (let i = 1; i <= H; i++) {
+  const row = lines[i].split('').map(ch => map[ch])
+  console.log(row.join(' '))
+}
+</script>
