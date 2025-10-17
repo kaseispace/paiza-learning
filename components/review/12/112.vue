487856211a43d9ad7c4d481ef6816a459f1601ab
yuki0829 <fledgling0829@gmail.com>
2025-10-17 14:59:22 +0900

    feat: 練習コード

---
+++ components/review/12/112.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const lines = [
+  '2 2 1',
+  '#.',
+  '.#',
+  '0 1',
+]
+const [H] = lines[0].split(' ').map(Number)
+const grid = lines.slice(1, 1 + H)
+const coords = lines.slice(1 + H).map(line => line.split(' ').map(Number))
+
+for (const [y, x] of coords) {
+  console.log(grid[y][x])
+}
+</script>
