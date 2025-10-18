defd7978fcbdac13e6a1e926809c84c0e56dc258
yuki0829 <fledgling0829@gmail.com>
2025-10-18 15:15:12 +0900

    feat: 練習コード

---
+++ components/review/12/113.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = [
+  '0 0 1',
+  'N',
+]
+let [y, x] = lines[0].split(' ').map(Number)
+const directions = lines.slice(1)
+
+for (const d of directions) {
+  if (d === 'N') y -= 1
+  else if (d === 'S') y += 1
+  else if (d === 'E') x += 1
+  else if (d === 'W') x -= 1
+
+  console.log(`${y} ${x}`)
+}
+</script>
