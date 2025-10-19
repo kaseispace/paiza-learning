f52c5f1c305a27081f81168fd0aa6b4e763fbae5
yuki0829 <fledgling0829@gmail.com>
2025-10-19 11:47:16 +0900

    feat: 練習コード

---
+++ components/review/12/114.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const lines = ['28446 39582']
+const [N, A] = lines[0].split(' ').map(Number)
+const targetRemainder = A % N
+
+for (let i = 1; i <= 100000; i++) {
+  if (i % N === targetRemainder) {
+    console.log(i)
+  }
+}
+</script>
