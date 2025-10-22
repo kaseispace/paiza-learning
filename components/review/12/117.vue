ade4566a583d00c90328faea5f49423092da36a6
yuki0829 <fledgling0829@gmail.com>
2025-10-22 20:48:11 +0900

    feat: 練習コード

---
+++ components/review/12/117.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const lines = [
+  '2',
+  'AB',
+  'BA',
+  'AA',
+  'BB',
+
+]
+const n = Number(lines[0])
+
+for (let i = 1; i <= n * 2; i += 2) {
+  const S = lines[i].split('')
+  const T = lines[i + 1].split('')
+
+  const s = S.sort().join('')
+  const t = T.sort().join('')
+
+  console.log(s == t ? 'Yes' : 'No')
+}
+
+// for (let i = 1; i <= n * 2; i += 2) {
+//   const S = lines[i]
+//   const T = lines[i + 1].split('')
+//   let result = true
+
+//   for (let j = 0; j < S.length; j++) {
+//     if (T.includes(S[j])) {
+//       const num = T.indexOf(S[j])
+//       T.splice(num, 1)
+//     }
+//     else {
+//       result = false
+//       break
+//     }
+//   }
+
+//   console.log(result ? 'Yes' : 'No')
+// }
+</script>
