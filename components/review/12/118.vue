9a9540efcebe713f8781060d3a86977fabd4d9d6
yuki0829 <fledgling0829@gmail.com>
2025-10-23 21:23:46 +0900

    feat: 練習コード

---
+++ components/review/12/118.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const lines = [
+  '5 4',
+  'A 4',
+  'A 4',
+  'B 2',
+  'A 2',
+]
+const [N, Q] = lines[0].split(' ').map(Number)
+const field: string[] = Array(N).fill(null)
+
+for (let i = 1; i <= Q; i++) {
+  const [P, xStr] = lines[i].split(' ')
+  const x = parseInt(xStr) - 1
+
+  if (field[x] === null) {
+    field[x] = P
+  }
+}
+
+const aCount = field.filter(v => v === 'A').length
+const bCount = field.filter(v => v === 'B').length
+
+if (aCount > bCount) {
+  console.log('A')
+}
+else if (bCount > aCount) {
+  console.log('B')
+}
+else {
+  console.log('Draw')
+}
+</script>
