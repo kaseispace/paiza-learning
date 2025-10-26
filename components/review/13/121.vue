9d89e69be0a7d56e818d93ef93e08cd4eafb6e18
yuki0829 <fledgling0829@gmail.com>
2025-10-26 11:39:28 +0900

    feat: 練習コード

---
+++ components/review/13/121.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = [
+  '3 2 1',
+  '4',
+  '5',
+  '2',
+]
+const [N, P, X] = lines[0].split(' ').map(Number)
+const A = lines.slice(1).map(Number)
+
+const index = (P <= N) ? P - 1 : N
+A.splice(index, 0, X)
+
+for (const value of A) {
+  console.log(value)
+}
+</script>
