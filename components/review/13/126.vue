fcb3242d19ec11698a87f7d29796b6a6628e36f4
yuki0829 <fledgling0829@gmail.com>
2025-10-31 14:35:51 +0900

    feat: 練習コード

---
+++ components/review/13/126.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const lines = ['10 10 5 5']
+const [H, W, Y, X] = lines[0].split(' ').map(Number)
+const board: string[][] = Array.from({ length: H }, () => Array(W).fill('.'))
+
+board[Y][X] = '!'
+const directions = [
+  [-1, -1],
+  [-1, 1],
+  [1, -1],
+  [1, 1],
+]
+
+for (const [dy, dx] of directions) {
+  let ny = Y + dy
+  let nx = X + dx
+
+  while (ny >= 0 && ny < H && nx >= 0 && nx < W) {
+    board[ny][nx] = '*'
+    ny += dy
+    nx += dx
+  }
+}
+
+for (const row of board) {
+  console.log(row.join(''))
+}
+</script>
