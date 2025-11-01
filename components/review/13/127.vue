5a87d80d9dcb56b5bf92a73f88741931910b8493
yuki0829 <fledgling0829@gmail.com>
2025-11-01 21:15:07 +0900

    feat: 練習コード

---
+++ components/review/13/127.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = ['3']
+const N = Number(lines[0])
+const MOD = 10 ** 10
+
+let dp1 = 1 // dp[0]
+let dp2 = 1 // dp[1]
+let result = 1
+
+for (let i = 2; i <= N; i++) {
+  result = (dp1 + dp2) % MOD
+  dp1 = dp2
+  dp2 = result
+}
+
+console.log(result % MOD)
+</script>
