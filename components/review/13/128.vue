be059342e6ebbeeafcfd51238531bded4ba57408
yuki0829 <fledgling0829@gmail.com>
2025-11-02 23:07:17 +0900

    feat: 練習コード

---
+++ components/review/13/128.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '/',
+  '\'',
+  '\'',
+  '/',
+  '_',
+]
+const walls = lines.slice(1)
+
+let dx = 1, dy = 0
+
+for (const wall of walls) {
+  if (wall === '_') {
+    // 方向変化なし
+  }
+  else if (wall === '/') {
+    [dx, dy] = [-dy, -dx]
+  }
+  else if (wall === '\\') {
+    [dx, dy] = [dy, dx]
+  }
+  console.log(`${dx} ${dy}`)
+}
+</script>
