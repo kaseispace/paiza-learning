a9f0371c2c5dd59db6a611cfbfc19b96fb0b047b
yuki0829 <fledgling0829@gmail.com>
2025-11-05 14:59:24 +0900

    feat: 練習コード

---
+++ components/review/14/131.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = ['1abc2efg345']
+const S = lines[0]
+
+const isDigit = (ch: string): boolean => ch >= '0' && ch <= '9'
+
+for (let i = 0; i < S.length; i++) {
+  if (!isDigit(S[i])) continue
+
+  for (let j = i; j < S.length; j++) {
+    if (isDigit(S[j])) {
+      console.log(S.slice(i, j + 1))
+    }
+  }
+}
+</script>
