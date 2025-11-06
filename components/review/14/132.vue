17730ad62c89207940e94c8e7861144f1099d775
yuki0829 <fledgling0829@gmail.com>
2025-11-06 21:28:31 +0900

    feat: 練習コード

---
+++ components/review/14/132.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+function main() {
+  const S = ['nopaizanostudy']
+
+  if (S.includes('paiza')) {
+    console.log('paiza')
+    return
+  }
+
+  const leetMap = [
+    ['p'],
+    ['a', '4', '@'],
+    ['i', '1', '!'],
+    ['z', '2'],
+    ['a', '4', '@'],
+  ]
+
+  for (let i = 0; i <= S.length - 5; i++) {
+    const slice = S.slice(i, i + 5)
+    let match = true
+    for (let j = 0; j < 5; j++) {
+      if (!leetMap[j].includes(slice[j])) {
+        match = false
+        break
+      }
+    }
+    if (match) {
+      console.log('leet')
+      return
+    }
+  }
+
+  console.log('nothing')
+}
+
+main()
+</script>
