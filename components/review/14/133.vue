d4f2a205fc422106a6903f1cbcac5e0d2f5ed502
yuki0829 <fledgling0829@gmail.com>
2025-11-07 22:54:59 +0900

    feat: 練習コード

---
+++ components/review/14/133.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const lines = ['172 8467 10']
+const [X, M, N] = lines[0].split(' ').map(Number)
+
+let sum = 0
+let power = 1
+
+for (let i = 1; i <= N; i++) {
+  power = (power * X) % M
+  sum = (sum + power) % M
+  console.log(sum)
+}
+</script>
