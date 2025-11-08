45505360849b75eebbbcbf7f11de906b6f613b00
yuki0829 <fledgling0829@gmail.com>
2025-11-08 15:17:23 +0900

    feat: 練習コード

---
+++ components/review/14/134.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const lines = [
+  '2 1',
+  '1 2',
+]
+const [N] = lines[0].split(' ').map(Number)
+const edges = lines.slice(1).map(line => line.split(' ').map(Number))
+
+const g: number[][] = Array.from({ length: N }, () => Array(N).fill(0))
+
+for (const [a, b] of edges) {
+  g[a - 1][b - 1] = 1
+  g[b - 1][a - 1] = 1
+}
+
+for (let i = 0; i < N; i++) {
+  console.log(g[i].join(''))
+}
+</script>
