21d47093a264b0d857f4a3226e6f49e5d58ffd0e
yuki0829 <fledgling0829@gmail.com>
2025-11-09 11:01:05 +0900

    feat: 練習コード

---
+++ components/review/14/135.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '1 2 3 4 5',
+  '1',
+  '1 4',
+]
+const A = lines[1].split(' ').map(Number)
+const Q = Number(lines[2])
+
+for (let k = 0; k < Q; k++) {
+  const [i, j] = lines[3 + k].split(' ').map(Number);
+  [A[i - 1], A[j - 1]] = [A[j - 1], A[i - 1]]
+}
+
+console.log(A.join(' '))
+</script>
