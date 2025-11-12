9e571ebec37ec3fb0f77f9050a8b209950cc3594
yuki0829 <fledgling0829@gmail.com>
2025-11-12 11:49:51 +0900

    feat: 練習コード

---
+++ components/review/14/138.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '1 2 3 4 2',
+]
+const N = Number(lines[0])
+const c = lines[1].split(' ').map(Number)
+
+let left = 0
+let maxLen = 0
+const seen: Set<number> = new Set()
+
+for (let right = 0; right < N; right++) {
+  while (seen.has(c[right])) {
+    seen.delete(c[left])
+    left++
+  }
+  seen.add(c[right])
+  maxLen = Math.max(maxLen, right - left + 1)
+}
+
+console.log(maxLen)
+</script>
