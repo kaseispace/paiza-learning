327d9e983b0117f31aa75573adc93da99507cdd1
yuki0829 <fledgling0829@gmail.com>
2025-11-13 11:37:48 +0900

    feat: 練習コード

---
+++ components/review/14/139.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const lines = ['174']
+let n = Number(lines[0])
+const output: number[] = []
+
+for (let i = 2; i * i <= n; i++) {
+  while (n % i === 0) {
+    output.push(i)
+    n = Math.floor(n / i)
+  }
+}
+
+if (n > 1) {
+  output.push(n)
+}
+
+console.log(output.join('\n'))
+</script>
