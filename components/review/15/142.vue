5731419961a7e32b202ba2919fd4aa0b2736361a
yuki0829 <fledgling0829@gmail.com>
2025-11-16 11:35:53 +0900

    feat: 練習コード

---
+++ components/review/15/142.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+type Student = {
+  height: number
+  name: string
+}
+
+const lines = [
+  '3',
+  '173 Hashimoto',
+  '195 Yamamoto',
+  '113 Yoshida',
+]
+const N = parseInt(lines[0], 10)
+const students: Student[] = []
+
+for (let i = 1; i <= N; i++) {
+  const [A, S] = lines[i].split(' ')
+  students.push({ height: parseInt(A, 10), name: S })
+}
+
+students.sort((a, b) => b.height - a.height)
+
+for (const st of students) {
+  console.log(st.name)
+}
+</script>
