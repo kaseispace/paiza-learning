4547588380854eedd6bc41a6974e8f9e39dd4ca3
yuki0829 <fledgling0829@gmail.com>
2025-11-19 11:36:54 +0900

    feat: 練習コード

---
+++ components/review/15/145.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const lines = [
+  '5 50',
+  '23',
+  '5',
+  '14',
+  '6',
+  '9',
+]
+const [N, M] = lines[0].split(' ').map(Number)
+let total = 0
+
+for (let i = 1; i <= N; i++) {
+  total += Number(lines[i])
+}
+
+if (total <= M) {
+  console.log('OK')
+}
+else {
+  console.log('NG')
+}
+</script>
