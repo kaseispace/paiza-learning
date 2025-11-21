d55cf4215119453df8e8b997c1611b62c1ae5d12
yuki0829 <fledgling0829@gmail.com>
2025-11-21 20:56:40 +0900

    feat: 練習コード

---
+++ components/review/15/147.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+  '5 3',
+  '8',
+  '2',
+  '5',
+  '6',
+  '1',
+]
+const [N, M] = lines[0].split(' ').map(Number)
+
+let count = 0
+for (let i = 1; i <= N; i++) {
+  const Ai = parseInt(lines[i], 10)
+  if (Ai <= M) {
+    count++
+  }
+}
+
+console.log(count)
+</script>
