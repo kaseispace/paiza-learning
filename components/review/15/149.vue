f18dbc50e032b1f37e710f138d70805ea22f9b54
yuki0829 <fledgling0829@gmail.com>
2025-11-23 11:28:40 +0900

    feat: 練習コード

---
+++ components/review/15/149.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const lines = [
+  '4 7',
+  '1 1',
+  '2 4',
+  '2 8',
+  '4 16',
+  '1 32',
+  '3 64',
+  '4 128',
+]
+const [N, M] = lines[0].split(' ').map(Number)
+const leds: number[] = Array(N).fill(0)
+
+for (let i = 1; i <= M; i++) {
+  const [a, b] = lines[i].split(' ').map(Number)
+  leds[a - 1] = b
+
+  if (leds.every(v => v > 0)) {
+    const sum = leds.reduce((acc, v) => acc + v, 0)
+    console.log(sum)
+    break
+  }
+}
+</script>
