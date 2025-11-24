e7454d1bed6673b890078c92c4fd45371779f4b3
yuki0829 <fledgling0829@gmail.com>
2025-11-24 21:38:13 +0900

    feat: 練習コード

---
+++ components/review/15/150.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '-1 2 -4 8 -16',
+]
+const N = Number(lines[0])
+const A = lines[1].split(' ').map(Number)
+
+let isArithmetic = true
+const d = A[1] - A[0]
+for (let i = 0; i < N - 1; i++) {
+  if (A[i + 1] - A[i] !== d) {
+    isArithmetic = false
+    break
+  }
+}
+
+let isGeometric = true
+const r = A[1] / A[0]
+for (let i = 0; i < N - 1; i++) {
+  if (A[i + 1] / A[i] !== r) {
+    isGeometric = false
+    break
+  }
+}
+
+console.log(isArithmetic ? 'Yes' : 'No')
+console.log(isGeometric ? 'Yes' : 'No')
+</script>
