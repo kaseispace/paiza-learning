5f858518ee6a2c47291c765c2759e457f67ff98b
yuki0829 <fledgling0829@gmail.com>
2025-11-26 21:37:26 +0900

    feat: 練習コード

---
+++ components/review/16/152.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const lines = [
+  '5 100',
+  '99',
+  '98',
+  '97',
+  '96',
+  '5',
+]
+const [N, X] = lines[0].split(' ').map(Number)
+const w = lines.slice(1).map(Number)
+
+let maxSum = 0
+
+for (let mask = 0; mask < (1 << N); mask++) {
+  let sum = 0
+  for (let i = 0; i < N; i++) {
+    if (mask & (1 << i)) sum += w[i]
+  }
+
+  if (sum <= X && sum > maxSum) {
+    maxSum = sum
+  }
+}
+
+console.log(maxSum)
+</script>
