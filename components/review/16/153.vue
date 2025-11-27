9346323b6db2fbf832034c1871f8c16b910a0974
yuki0829 <fledgling0829@gmail.com>
2025-11-27 11:47:15 +0900

    feat: 練習コード

---
+++ components/review/16/153.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = [
+  '3 3',
+  '1 2 3',
+]
+const [n, k] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+
+const MOD = 1000000000
+
+const dp: number[] = Array(k + 1).fill(0)
+dp[0] = 1
+
+for (let i = 0; i < n; i++) {
+  const x = a[i]
+  for (let s = k; s >= x; s--) {
+    dp[s] = (dp[s] + dp[s - x]) % MOD
+  }
+}
+
+console.log(dp[k] % MOD)
+</script>
