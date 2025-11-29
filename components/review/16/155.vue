d6c542f93ea5e5e7d1206b61b5071addcb8f7087
yuki0829 <fledgling0829@gmail.com>
2025-11-29 16:15:57 +0900

    feat: 練習コード

---
+++ components/review/16/155.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const lines = [
+  '3 3',
+  '###',
+  '...',
+  '###',
+]
+const [H, W] = lines[0].split(' ').map(Number)
+const S = lines.slice(1)
+
+for (let y = 0; y < H; y++) {
+  for (let x = 0; x < W; x++) {
+    let ok = false
+    if (y === 0) {
+      if (S[y + 1][x] === '#') ok = true
+    }
+    else if (y === H - 1) {
+      if (S[y - 1][x] === '#') ok = true
+    }
+    else if (S[y - 1][x] === '#' && S[y + 1][x] === '#') ok = true
+    if (ok) {
+      console.log(`${y} ${x}`)
+    }
+  }
+}
+</script>
