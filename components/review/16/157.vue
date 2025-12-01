3b1252ca28e572f2b54a397cc56cec7fc04fbce7
yuki0829 <fledgling0829@gmail.com>
2025-12-01 20:26:52 +0900

    feat: 練習コード

---
+++ components/review/16/157.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const lines = ['3 1048 1']
+const [A, B, C] = lines[0].split(' ').map(Number)
+
+if (A % B === C) {
+  const q = Math.floor(A / B)
+  console.log(`1 ${-q}`)
+}
+else {
+  const q = Math.floor(B / A)
+  console.log(`${-q} 1`)
+}
+</script>
