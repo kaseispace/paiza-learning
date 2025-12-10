bbf27689e512b439148c0435e6d2ae254a5c0075
yuki0829 <fledgling0829@gmail.com>
2025-12-10 21:50:25 +0900

    feat: 練習コード

---
+++ components/review/17/166.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const lines = [
+  '10 10 1',
+  '2 2 4',
+]
+const [H, W] = lines[0].split(' ').map(Number)
+const [h, w, x] = lines[1].split(' ').map(Number)
+
+const field: string[][] = Array.from({ length: H }, () => Array(W).fill('.'))
+
+for (let row = H - h; row < H; row++) {
+  for (let col = x; col < x + w; col++) {
+    field[row][col] = '#'
+  }
+}
+
+for (let row = 0; row < H; row++) {
+  console.log(field[row].join(''))
+}
+</script>
