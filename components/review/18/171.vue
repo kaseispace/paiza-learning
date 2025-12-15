f47e55b09ae4e5d330ee6d31679d6f3bd0471c12
yuki0829 <fledgling0829@gmail.com>
2025-12-15 15:17:25 +0900

    feat: 練習コード

---
+++ components/review/18/171.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  'shirt 500',
+  'pants 1000',
+  'pants 500',
+  'shoes 2000',
+]
+const n = Number(lines[0])
+
+let total = 0
+let hasPants = false
+
+for (let i = 1; i <= n; i++) {
+  const [c, pStr] = lines[i].split(' ')
+  const p = Number(pStr)
+  total += p
+  if (c === 'pants') hasPants = true
+}
+
+if (hasPants && total >= 2000) {
+  total -= 500
+}
+
+console.log(total)
+</script>
