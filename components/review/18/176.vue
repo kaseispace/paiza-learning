8091663f71fea6aef5d81e262b3ddcce46b6e76a
yuki0829 <fledgling0829@gmail.com>
2025-12-20 14:40:00 +0900

    feat: 練習コード

---
+++ components/review/18/176.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const lines = [
+  '9 3',
+  '7989',
+  '98693',
+  '10250',
+  '-89388',
+  '-32127',
+  '34027',
+  '27383',
+  '6151',
+  '-20489',
+]
+const [N, D] = lines[0].split(' ').map(Number)
+const A = lines.slice(1).map(Number)
+
+for (let i = 0; i < N; i += D) {
+  let maxVal = -Infinity
+  for (let j = i; j < i + D; j++) {
+    if (A[j] > maxVal) maxVal = A[j]
+  }
+  console.log(maxVal)
+}
+</script>
