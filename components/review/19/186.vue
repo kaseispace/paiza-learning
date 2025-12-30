fff23a9153364815fad8e8536b97b569262aeacb
yuki0829 <fledgling0829@gmail.com>
2025-12-30 10:28:01 +0900

    feat: 練習コード

---
+++ components/review/19/186.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const lines = [
+  '6',
+  'ahoxx hogexx zzho geho hogexx ho',
+
+]
+const S = lines[1].split(' ').join('')
+let count = 0
+
+const target = 'hoge'
+let tIndex = 0
+
+for (let i = 0; i < S.length; i++) {
+  if (S[i] === target[tIndex]) {
+    tIndex++
+
+    if (tIndex === target.length) {
+      count++
+      tIndex = 0
+    }
+  }
+}
+
+console.log(count)
+</script>
