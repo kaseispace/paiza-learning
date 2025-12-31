4436eeefc00a39fe864112abd1c7a79d28c3a981
yuki0829 <fledgling0829@gmail.com>
2025-12-31 10:39:57 +0900

    feat: 練習コード

---
+++ components/review/19/187.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const lines = [
+  '5 7',
+  '2 7 8 8 9',
+  '3 3 5 6 7 7 9',
+]
+const [n, m] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+const b = lines[2].split(' ').map(Number)
+
+a.sort((x, y) => x - y)
+b.sort((x, y) => x - y)
+
+let result = true
+let j = 0
+
+for (let i = 0; i < n; i++) {
+  while (j < m && b[j] < a[i]) {
+    j++
+  }
+
+  if (j == m) {
+    result = false
+    break
+  }
+
+  j++
+}
+
+console.log(result ? 'YES' : 'NO')
+</script>
