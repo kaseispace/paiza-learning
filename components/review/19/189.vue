d3222c3802a728edad806c6dc5f5c55a29390d84
yuki0829 <fledgling0829@gmail.com>
2026-01-02 20:54:47 +0900

    feat: 練習コード

---
+++ components/review/19/189.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+ type Player = {
+   num: number
+   name: string
+ }
+
+const lines = [
+  '5',
+  '31 SAITO',
+  '56 GORO',
+  '11 IIJIMA',
+  '70 NAKAMURA',
+  '1 HAJIME',
+]
+const n = Number(lines[0])
+const players: Player[] = []
+
+for (let i = 1; i <= n; i++) {
+  const [b, p] = lines[i].split(' ')
+  players.push({ num: Number(b), name: p })
+}
+
+players.sort((a, b) => a.num - b.num)
+
+players.forEach((player) => {
+  console.log(player.num, player.name)
+})
+</script>
