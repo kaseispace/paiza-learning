e972a7457e5d650020126445568e33a1f5562444
yuki0829 <fledgling0829@gmail.com>
2026-01-03 20:35:13 +0900

    feat: 練習コード

---
+++ components/review/19/190.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const lines = [
+  '3',
+  'a',
+  'shift b',
+  'c',
+]
+const n = Number(lines[0])
+let caps = false
+let result = ''
+
+for (let i = 1; i <= n; i++) {
+  const parts = lines[i].split(' ')
+
+  if (parts[0] === 'shift') {
+    result += parts[1].toUpperCase()
+  }
+  else if (parts[0] === 'capslock') {
+    caps = !caps
+  }
+  else {
+    result += caps ? parts[0].toUpperCase() : parts[0]
+  }
+}
+
+console.log(result)
+</script>
