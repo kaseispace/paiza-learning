4ca13406536e365f4fe62a10d959a43a7f3e9ec6
yuki0829 <fledgling0829@gmail.com>
2026-01-04 15:44:31 +0900

    feat: 練習コード

---
+++ components/review/20/191.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '183',
+  '138',
+  '183',
+  '138',
+]
+
+const n = Number(lines[0])
+const a = lines.slice(1).map(Number)
+let result = true
+
+for (let i = 1; i < n; i++) {
+  if (a[i - 1] > a[i]) {
+    result = false
+    break
+  }
+}
+
+console.log(result ? 'Yes' : 'No')
+</script>
