bceddc1a7c2f0d860de80fd1f06eee180b4148f6
yuki0829 <fledgling0829@gmail.com>
2026-01-05 11:37:06 +0900

    feat: 練習コード

---
+++ components/review/20/192.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '3',
+  '2',
+  '0',
+  '1',
+]
+const result: boolean[] = Array(4).fill(false)
+
+for (let i = 0; i < 5; i++) {
+  const hit = Number(lines[i])
+  if (hit >= 1 && hit <= 4) {
+    result[hit - 1] = true
+  }
+}
+
+console.log(result.includes(false) ? 'No' : 'Yes')
+</script>
