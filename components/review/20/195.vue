01fb6346a0b83ba14c9ff067df47e4cc36442816
yuki0829 <fledgling0829@gmail.com>
2026-01-08 11:06:21 +0900

    feat: 練習コード

---
+++ components/review/20/195.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '0 2 1 8',
+  '6 3 2 0',
+]
+const n = Number(lines[0])
+const a = lines[1].split(' ').map(Number)
+const b = lines[2].split(' ').map(Number)
+
+let maxCap = 0
+let current = 0
+
+for (let i = 0; i < n; i++) {
+  current -= a[i]
+  current += b[i]
+
+  maxCap = Math.max(current, maxCap)
+}
+
+console.log(maxCap)
+</script>
