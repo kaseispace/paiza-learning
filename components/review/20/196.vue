eabbed4d7827e053f6ef7b890a34ae91ff15daa2
yuki0829 <fledgling0829@gmail.com>
2026-01-09 11:45:26 +0900

    feat: 練習コード

---
+++ components/review/20/196.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = ['pppazzzaipza']
+const s = lines[0]
+let index = 0
+const word = 'paiza'
+
+for (let i = 0; i < s.length; i++) {
+  if (word[index] === s[i]) {
+    index++
+  }
+
+  if (word.length === index) break
+}
+
+console.log(word.length === index ? 'Yes' : 'No')
+</script>
