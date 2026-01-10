0791f43b20a0947c54be6ce616a516653211f224
yuki0829 <fledgling0829@gmail.com>
2026-01-10 11:30:27 +0900

    feat: 練習コード

---
+++ components/review/20/197.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const lines = [
+  '2',
+  '0 1',
+  '1 2',
+]
+const n = Number(lines[0])
+const a = lines.slice(1).map(edge => edge.split(' ').map(Number))
+const result: boolean[] = Array(2).fill(false)
+let result2 = false
+
+for (let i = 0; i < n; i++) {
+  const edge = a[i]
+
+  if (edge[0] === 0 && edge[1] === 1) {
+    result[0] = true
+  }
+  else if (edge[0] === 1 && edge[1] === 2) {
+    result[1] = true
+  }
+
+  if (edge[0] === 0 && edge[1] === 2) {
+    result2 = true
+  }
+
+  if (!result.includes(false)) break
+}
+
+console.log(!result.includes(false) || result2 ? 'Yes' : 'No')
+</script>
