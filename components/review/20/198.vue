7ba24c46817009bb301a4e3764f6182aa3420380
yuki0829 <fledgling0829@gmail.com>
2026-01-11 21:30:53 +0900

    feat: 練習コード

---
+++ components/review/20/198.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+  '2',
+  'aaa',
+  'bbb',
+  'aabbccddee',
+]
+const N = Number(lines[0])
+const S = lines.slice(1, N + 1)
+const T = lines[N + 1]
+let result = false
+
+for (let i = 0; i < N; i++) {
+  if (T.includes(S[i])) {
+    result = true
+    break
+  }
+}
+
+console.log(result ? 'Yes' : 'No')
+</script>
