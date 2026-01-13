9e0da1d86aa891d015e94811f5f2a0421cc012d3
yuki0829 <fledgling0829@gmail.com>
2026-01-13 11:42:21 +0900

    feat: 練習コード

---
+++ components/review/20/200.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  'Jimmy',
+  'Bob',
+  'James',
+  'Jimmy',
+]
+const n = Number(lines[0])
+const result: Set<string> = new Set()
+
+for (let i = 1; i <= n; i++) {
+  const c = lines[i]
+  if (!result.has(c)) {
+    result.add(c)
+    console.log('YES')
+  }
+  else {
+    console.log('NO')
+  }
+}
+</script>
