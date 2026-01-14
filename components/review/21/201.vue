adcd78b2c52ce35dfa017468e235e82cb6657da0
yuki0829 <fledgling0829@gmail.com>
2026-01-14 11:07:18 +0900

    feat: 練習コード

---
+++ components/review/21/201.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const lines = [
+  '3 2',
+  '1 0 1',
+  '0 1 0',
+]
+const [n, m] = lines[0].split(' ').map(Number)
+const c = lines.slice(1).map(score => score.split(' ').map(Number))
+let result = false
+
+for (let i = 0; i < n; i++) {
+  let score = 0
+  for (let j = 0; j < m; j++) {
+    score += c[j][i]
+  }
+
+  if (score === m) {
+    result = true
+    break
+  }
+}
+
+console.log(result ? 'Yes' : 'No')
+</script>
