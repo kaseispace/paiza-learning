48c7c58fdea8fea702ee79f77ff5b481dc9f62cc
yuki0829 <fledgling0829@gmail.com>
2026-01-15 11:04:51 +0900

    feat: 練習コード

---
+++ components/review/21/202.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = [
+  '4 5.0',
+  '5.0',
+  '5.0',
+  '7.5',
+  '2.5',
+]
+const [, k] = lines[0].split(' ').map(Number)
+const hp = lines.slice(1).map(Number)
+let sum = 0
+
+hp.forEach(num => sum += num)
+const result = Math.ceil(sum / k)
+
+console.log(result)
+</script>
