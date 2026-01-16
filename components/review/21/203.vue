598bb1171ac49cfefffa00e9565641ef5d75fe7c
yuki0829 <fledgling0829@gmail.com>
2026-01-16 11:41:49 +0900

    feat: 練習コード

---
+++ components/review/21/203.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '6167',
+  '3819',
+  '343',
+  '554',
+  '432',
+]
+const n = Number(lines[0])
+const result: number[] = []
+
+for (let i = 1; i <= n; i++) {
+  const num = Number(lines[i])
+  if (num % 2 !== 0) {
+    result.push(num)
+  }
+}
+
+result.sort((a, b) => a - b).forEach(num => console.log(num))
+</script>
