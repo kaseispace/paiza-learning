fbe77db7832e95b03a96fa12c5dc2ea75c7b5c4a
yuki0829 <fledgling0829@gmail.com>
2026-01-20 11:09:36 +0900

    feat: 練習コード

---
+++ components/review/21/207.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const lines = [
+  '5 6',
+  '4 3 6 1 3',
+  '3',
+  '1 3',
+  '1 5',
+  '2 3',
+]
+const [, M] = lines[0].split(' ').map(Number)
+const tree = lines[1].split(' ').map(Number)
+const queries = lines.slice(3).map(line => line.split(' ').map(Number))
+
+for (const [s, e] of queries) {
+  const startIndex = s - 1
+  const endIndex = e - 1
+  const length = endIndex - startIndex + 1
+
+  const sum = tree.slice(startIndex, endIndex + 1).reduce((acc, cur) => acc + cur, 0)
+  const avg = sum / length
+
+  if (avg < M) {
+    const add = Math.ceil(M - avg)
+    for (let i = startIndex; i <= endIndex; i++) {
+      tree[i] += add
+    }
+  }
+}
+
+console.log(tree.join(' '))
+</script>
