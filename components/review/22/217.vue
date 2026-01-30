5eac92c4be8462c0c4cdf4ed0f5d919460c32d2b
yuki0829 <fledgling0829@gmail.com>
2026-01-30 14:57:18 +0900

    feat: 練習コード

---
+++ components/review/22/217.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const lines = [
+  '6 6',
+  '3',
+  '3 5 1 abc',
+  '1 1 0 xyz',
+  '6 2 1 hi',
+
+]
+const [H, W] = lines[0].split(' ').map(Number)
+const N = Number(lines[1])
+const grid: string[][] = Array.from({ length: H }, () => Array(W).fill('.'))
+
+for (let i = 2; i < N + 2; i++) {
+  const [r, c, b, S] = lines[i].split(' ')
+  const R = Number(r) - 1
+  const C = Number(c) - 1
+  const B = Number(b)
+
+  if (B === 0) {
+    for (let k = 0; k < S.length; k++) {
+      grid[R + k][C] = S[k]
+    }
+  }
+  else {
+    for (let k = 0; k < S.length; k++) {
+      grid[R][C + k] = S[k]
+    }
+  }
+}
+
+grid.forEach(row => console.log(row.join('')))
+</script>
