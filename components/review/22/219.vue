eeeea35d45e68bc3adc9d6bc0d001f03af6737ae
yuki0829 <fledgling0829@gmail.com>
2026-02-01 10:10:41 +0900

    feat: 練習コード

---
+++ components/review/22/219.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+  '6',
+  '1 2 3 4 5 6',
+
+]
+const A = lines[1].split(' ').map(Number)
+
+const S = A.reduce((a, b) => a + b, 0)
+const max = S
+
+const dp: boolean[] = Array(max + 1).fill(false)
+dp[0] = true
+
+for (const w of A) {
+  for (let i = max; i >= w; i--) {
+    if (dp[i - w]) dp[i] = true
+  }
+}
+
+let best = 0
+for (let i = 0; i <= S; i++) {
+  if (dp[i] && Math.abs(S - 2 * i) < Math.abs(S - 2 * best)) {
+    best = i
+  }
+}
+
+console.log(Math.abs(S - 2 * best))
+</script>
