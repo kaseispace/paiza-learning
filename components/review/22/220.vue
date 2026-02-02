68b72d3c017faef1cc92c7f4df2f151684dcecf6
yuki0829 <fledgling0829@gmail.com>
2026-02-02 10:28:37 +0900

    feat: 練習コード

---
+++ components/review/22/220.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = ['5', '3 3 3 3 3', 'abc def ghi jkl mno']
+const N = Number(lines[0])
+const S = lines[2].split(' ')
+const result: Set<string> = new Set()
+
+for (let i = 0; i < N; i++) {
+  S[i] = S[i][0] + S[i][S[i].length - 1]
+}
+
+for (let i = 0; i < N; i++) {
+  const w = S[i]
+
+  for (let j = 0; j < N; j++) {
+    if (w[0] === S[j][0]) {
+      const wSum = S[j][1] + w[1]
+
+      for (let k = 0; k < N; k++) {
+        if (wSum === S[k]) {
+          const key = [i + 1, j + 1, k + 1].sort((a, b) => a - b).join('')
+          result.add(key)
+        }
+      }
+    }
+  }
+}
+
+console.log(result.size)
+</script>
