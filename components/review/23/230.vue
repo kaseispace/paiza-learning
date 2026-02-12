4e667da8bb35c3068e98c0ab3dfc3609c904eb87
yuki0829 <fledgling0829@gmail.com>
2026-02-12 21:02:34 +0900

    feat: 練習コード

---
+++ components/review/23/230.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const lines = ['3']
+const n = Number(lines[0].trim())
+
+const used: boolean[] = Array(n + 1).fill(false)
+const perm: number[] = []
+const out: string[] = []
+
+const dfs = () => {
+  if (perm.length === n) {
+    out.push(perm.join(' '))
+    return
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (!used[i]) {
+      used[i] = true
+      perm.push(i)
+      dfs()
+      perm.pop()
+      used[i] = false
+    }
+  }
+}
+
+dfs()
+console.log(out.join('\n'))
+</script>
