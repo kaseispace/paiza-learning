c5e7ca9dd4c41bbf3d687fc733ce6be4ddfa341c
yuki0829 <fledgling0829@gmail.com>
2026-02-17 14:46:35 +0900

    feat: 練習コード

---
+++ components/review/24/235.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const lines = [
+  '4 2',
+  '5 3 8 2',
+]
+const [n, k] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+
+let sum = 0
+for (let i = 0; i < k; i++) sum += a[i]
+
+let maxSum = sum
+
+for (let i = 1; i + k - 1 < n; i++) {
+  sum = sum - a[i - 1] + a[i + k - 1]
+  if (sum > maxSum) maxSum = sum
+}
+
+console.log(maxSum)
+</script>
