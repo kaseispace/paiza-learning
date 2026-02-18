766dfc8abc5e95d56dfc5dcd20ad35cfc44e147b
yuki0829 <fledgling0829@gmail.com>
2026-02-18 15:26:34 +0900

    feat: 練習コード

---
+++ components/review/24/236.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+  '1 1 0',
+  '1 0 0',
+]
+const [n, m] = lines[0].split(' ').map(Number)
+
+const data = lines.slice(1).map(line => line.split(' ').map(Number))
+
+let existsAllWrong = false
+
+for (let j = 0; j < n; j++) {
+  let allZero = true
+
+  for (let i = 0; i < m; i++) {
+    if (data[i][j] === 1) {
+      allZero = false
+      break
+    }
+  }
+
+  if (allZero) {
+    existsAllWrong = true
+    break
+  }
+}
+
+console.log(existsAllWrong ? 'Yes' : 'No')
+</script>
