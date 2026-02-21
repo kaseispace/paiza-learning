70279a81ce05af004698e72da87c0c41eace7ad6
yuki0829 <fledgling0829@gmail.com>
2026-02-21 11:35:10 +0900

    feat: 練習コード

---
+++ components/review/24/239.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const lines = ['5 3']
+const [x, y] = lines[0].split(' ').map(Number)
+
+const dx = Math.abs(x)
+const dy = Math.abs(y)
+
+const diff = Math.abs(dx - dy)
+
+const extra = Math.max(0, diff - 1)
+
+console.log(dx + dy + extra)
+</script>
