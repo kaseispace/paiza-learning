fa350380ac0108f9dc76954b661594beaed8ebca
yuki0829 <fledgling0829@gmail.com>
2026-02-24 16:06:16 +0900

    feat: 練習コード

---
+++ components/review/25/242.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = ['11 7 2']
+const [x, y, z] = lines[0].split(' ').map(Number)
+let k
+
+if (x - z >= 0) {
+  k = Math.ceil((x - z) / y)
+}
+else {
+  k = 0
+}
+
+const n = y * k + z
+
+console.log(n)
+</script>
