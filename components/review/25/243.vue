26cb3a82cb7cbb31307234e5aefc46a62ee09658
yuki0829 <fledgling0829@gmail.com>
2026-02-25 10:43:29 +0900

    feat: 練習コード

---
+++ components/review/25/243.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const lines = ['red green blue blue green blue']
+const words = lines[0].trim().split(' ')
+
+const freq: Map<string, number> = new Map()
+
+for (const w of words) {
+  if (freq.has(w)) {
+    freq.set(w, freq.get(w)! + 1)
+  }
+  else {
+    freq.set(w, 1)
+  }
+}
+
+for (const [word, count] of freq) {
+  console.log(word + ' ' + count)
+}
+</script>
