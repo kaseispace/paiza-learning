b062c8277ae28405402b6385abfdb427cdb8b267
yuki0829 <fledgling0829@gmail.com>
2026-02-26 14:46:11 +0900

    feat: 練習コード

---
+++ components/review/25/244.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+  '4 2',
+  '5 3 8 2',
+]
+const [n, k] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+
+let sum = 0
+for (let i = 0; i < k; i++) {
+  sum += a[i]
+}
+
+let ans = sum
+for (let i = 0; i + k < n; i++) {
+  sum = sum - a[i] + a[i + k]
+  if (sum > ans) ans = sum
+}
+
+console.log(ans)
+</script>
