75b6bdf8db9e115ac6a9677c9e2f3bf505808e2b
yuki0829 <fledgling0829@gmail.com>
2026-02-27 15:25:37 +0900

    feat: 練習コード

---
+++ components/review/25/245.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+  '3 5',
+  '5',
+  '4',
+  '4',
+
+]
+const [N, X] = lines[0].split(' ').map(Number)
+
+let maxPrice = 0
+
+for (let i = 1; i <= N; i++) {
+  const price = Number(lines[i])
+  if (price <= X && price > maxPrice) {
+    maxPrice = price
+  }
+}
+
+console.log(X - maxPrice)
+</script>
