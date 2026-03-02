8be4184a30881889e823cf188874b1a834f8c10a
yuki0829 <fledgling0829@gmail.com>
2026-03-02 15:21:59 +0900

    feat: 練習コード

---
+++ components/review/25/248.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const lines = [
+  '1 10',
+  '9',
+
+]
+const [N, M] = lines[0].split(' ').map(Number)
+const A = lines[1].split(' ').map(Number)
+
+let l = 0
+let sum = 0
+let ans = Infinity
+
+for (let r = 0; r < N; r++) {
+  sum += A[r]
+
+  while (sum >= M) {
+    ans = Math.min(ans, r - l + 1)
+    sum -= A[l]
+    l++
+  }
+}
+
+console.log(ans === Infinity ? -1 : ans)
+</script>
