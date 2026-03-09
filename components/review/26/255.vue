a3b824a1def104299145ecc4c3321d68a96b5a1a
yuki0829 <fledgling0829@gmail.com>
2026-03-09 11:21:26 +0900

    feat: 練習コード

---
+++ components/review/26/255.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '2',
+  '1 6',
+  '1 5',
+  '1 9',
+
+]
+const Q = Number(lines[0])
+
+const A: number[] = []
+
+for (let i = 1; i <= Q; i++) {
+  const parts = lines[i].split(' ')
+  const type = Number(parts[0])
+
+  if (type === 1) {
+    const x = Number(parts[1])
+    A.push(x)
+  }
+}
+
+console.log(A.length)
+for (const v of A) {
+  console.log(v)
+}
+</script>
