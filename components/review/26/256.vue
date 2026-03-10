91f779f875843c02c2da8cdc61caff13fe86c3a9
yuki0829 <fledgling0829@gmail.com>
2026-03-10 10:22:29 +0900

    feat: 練習コード

---
+++ components/review/26/256.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const lines = [
+  '5 2',
+  '3',
+  '8',
+  '1',
+  '9',
+  '4',
+
+]
+const [N, K] = lines[0].split(' ').map(Number)
+
+const A: number[] = []
+for (let i = 0; i < N; i++) {
+  A.push(Number(lines[1 + i]))
+}
+
+const remain = N - K
+
+for (let i = 0; i < remain; i++) {
+  console.log(A[i])
+}
+</script>
