9f743d68d256ec8eae3370154849e241a9aebbea
yuki0829 <fledgling0829@gmail.com>
2026-03-13 15:06:52 +0900

    feat: 練習コード

---
+++ components/review/26/259.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const lines = [
+  '6 2 6',
+  '6 5 4 3 2 1',
+
+]
+const [, l, r] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+
+const part = a.slice(l - 1, r - 1)
+
+part.sort((x, y) => x - y)
+
+for (let i = l - 1; i < r - 1; i++) {
+  a[i] = part[i - (l - 1)]
+}
+
+console.log(a.join(' '))
+</script>
