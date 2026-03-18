bde4edf4d6f37280d8ebdb8ae61e92e8b29b7b2a
yuki0829 <fledgling0829@gmail.com>
2026-03-18 21:53:15 +0900

    feat: 練習コード

---
+++ components/review/27/264.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const lines = [
+  '3 5',
+  '5',
+  '5',
+  '5',
+]
+const [N, X] = lines[0].split(' ').map(Number)
+const x: number[] = []
+
+for (let i = 0; i < N; i++) {
+  x.push(Number(lines[1 + i]))
+}
+
+x.sort((a, b) => a - b)
+
+let sum = 0
+let cnt = 0
+
+for (let i = 0; i < N; i++) {
+  if (sum + x[i] > X) break
+  sum += x[i]
+  cnt++
+}
+
+console.log(cnt)
+</script>
