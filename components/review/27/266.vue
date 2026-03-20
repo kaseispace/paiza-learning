bcad4bd00b87ef28bd11bda516eac6b10b83037e
yuki <fledgling0829@gmail.com>
2026-03-20 10:27:06 +0900

    feat: 練習コード

---
+++ components/review/27/266.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const lines = [
+  '2 4 1 3 6 5',
+  '5',
+]
+
+const [T, B, U, D, L, R] = lines[0].split(' ').map(Number)
+const Q = Number(lines[1])
+let ans = 0
+
+if (Q === T) ans = B
+else if (Q === B) ans = T
+else if (Q === U) ans = D
+else if (Q === D) ans = U
+else if (Q === L) ans = R
+else if (Q === R) ans = L
+
+console.log(ans)
+</script>
