688faaa731f03f40f203cb075823746f4e25a0bc
yuki <fledgling0829@gmail.com>
2026-03-23 10:26:51 +0900

    feat: 練習コード

---
+++ components/review/27/269.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const lines = [
+  '3 4',
+  '8 1 3 8',
+  '1 3 8 1',
+  '1 1 1 1',
+  '1 3 8 1',
+
+]
+const [m, n] = lines[0].split(' ').map(Number)
+const A: number[][] = []
+
+for (let i = 0; i < m; i++) {
+  A.push(lines[1 + i].split(' ').map(Number))
+}
+
+const X = lines[1 + m].split(' ').map(Number)
+
+for (let i = 0; i < m; i++) {
+  let same = true
+
+  for (let j = 0; j < n; j++) {
+    if (A[i][j] !== X[j]) {
+      same = false
+      break
+    }
+  }
+
+  if (same) {
+    console.log(i + 1)
+    break
+  }
+}
+</script>
