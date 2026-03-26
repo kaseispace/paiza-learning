476a9e4c1f1eccbc77b21cb8a867451bd63b0a9e
yuki <fledgling0829@gmail.com>
2026-03-26 10:45:32 +0900

    feat: 練習コード

---
+++ components/review/28/272.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const lines = [
+  '4 3 3 9',
+  '1,4,7',
+  '2,5,8',
+  '3,6,9',
+  '10,11,12',
+]
+const [n, , k, x] = lines[0].split(' ').map(Number)
+
+for (let i = 0; i < n; i++) {
+  const row = lines[1 + i]
+  const cols = row.split(',').map(Number)
+
+  if (cols[k - 1] >= x) {
+    console.log(row)
+  }
+}
+</script>
