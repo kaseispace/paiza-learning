422fe13fda365506885b568c692688a30491414d
yuki <fledgling0829@gmail.com>
2026-03-29 09:23:45 +0900

    feat: 練習コード

---
+++ components/review/28/275.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+  '0.0001',
+  '2',
+  '10000 0',
+  '10000 1',
+]
+const aGiven = lines[0]
+const n = Number(lines[1])
+
+let sumXY = 0
+let sumXX = 0
+
+for (let i = 0; i < n; i++) {
+  const [x, y] = lines[2 + i].split(' ').map(Number)
+  sumXY += x * y
+  sumXX += x * x
+}
+
+const alpha = sumXY / sumXX
+const alphaRounded = alpha.toFixed(4)
+
+if (alphaRounded === aGiven) {
+  console.log('Yes')
+}
+else {
+  console.log('No')
+}
+</script>
