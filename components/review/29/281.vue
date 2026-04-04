65227c9439cbd8b648a67fef28246a21ecc79160
yuki <fledgling0829@gmail.com>
2026-04-04 09:20:37 +0900

    feat: 練習コード

---
+++ components/review/29/281.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = ['3 1 2']
+const [, s, k] = lines[0].split(' ').map(Number)
+
+let t = 1
+if (t === s) t = 2
+
+const path: number[] = []
+path.push(s)
+
+for (let i = 0; i < k; i++) {
+  const last = path[path.length - 1]
+  if (last === s) {
+    path.push(t)
+  }
+  else {
+    path.push(s)
+  }
+}
+
+console.log(path.join(' '))
+</script>
