48f14fdc9e2667772f46e55613b8ad7ed051fd2e
yuki <fledgling0829@gmail.com>
2026-04-10 09:11:20 +0900

    feat: 練修コード

---
+++ components/review/29/287.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '3',
+  '5',
+  '19',
+  '1',
+  '2',
+]
+const n = Number(lines[0])
+const arr: number[] = []
+
+for (let i = 1; i <= n; i++) {
+  arr.push(Number(lines[i]))
+}
+
+arr.sort((a, b) => a - b)
+
+for (const x of arr) {
+  console.log(x)
+}
+</script>
