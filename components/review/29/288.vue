15a6a830f7ed6c1b2f6886d3c0db71f69ea524e1
yuki <fledgling0829@gmail.com>
2026-04-11 19:50:24 +0900

    feat: 練習コード

---
+++ components/review/29/288.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const lines = [
+  '3 6',
+  '1 2 3',
+]
+const [N, K] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+
+const total = 1 << N
+let ok = false
+
+for (let mask = 0; mask < total; mask++) {
+  let sum = 0
+
+  for (let i = 0; i < N; i++) {
+    if (mask & (1 << i)) {
+      sum += a[i]
+    }
+  }
+
+  if (sum === K) {
+    ok = true
+    break
+  }
+}
+
+console.log(ok ? 'Yes' : 'No')
+</script>
