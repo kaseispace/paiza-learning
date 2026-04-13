e84a59c16bd7c17568879e7e6be6530fa608ea0c
yuki <fledgling0829@gmail.com>
2026-04-13 09:28:15 +0900

    feat: 練習コード

---
+++ components/review/29/290.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const lines = ['1']
+const N = Number(lines[0])
+let s = '0'
+
+for (let i = 2; i <= N; i++) {
+  const rev = s.split('').reverse()
+  const inv = rev.map(c => (c === '0' ? '1' : '0')).join('')
+  s = s + '0' + inv
+}
+
+console.log(s)
+</script>
