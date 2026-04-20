0bd02d5c9145a617c11a6b91f6fbd6a95ba61a83
yuki <fledgling0829@gmail.com>
2026-04-20 09:33:12 +0900

    feat: 練習コード

---
+++ components/review/30/297.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const lines = [
+  '1 1',
+  '#',
+]
+const [H, W] = lines[0].split(' ').map(Number)
+
+for (let y = 0; y < H; y++) {
+  const row = lines[1 + y]
+
+  for (let x = 0; x < W; x++) {
+    if (row[x] === '#') {
+      console.log(y, x)
+      process.exit(0)
+    }
+  }
+}
+</script>
