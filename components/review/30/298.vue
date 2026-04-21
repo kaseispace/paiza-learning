c041d7180970c98c83e75cc5788fb053388b6651
yuki <fledgling0829@gmail.com>
2026-04-21 09:08:14 +0900

    feat: 練習コード

---
+++ components/review/30/298.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const lines = ['0']
+const N = BigInt(lines['0'])
+const MOD = 1000003n
+
+function modPow(base: bigint, exp: bigint, mod: bigint) {
+  let result = 1n
+  let b = base % mod
+  let e = exp
+
+  while (e > 0n) {
+    if (e & 1n) {
+      result = (result * b) % mod
+    }
+
+    b = (b * b) % mod
+    e >>= 1n
+  }
+
+  return result
+}
+
+const ans = modPow(2n, N, MOD)
+console.log(ans.toString())
+</script>
