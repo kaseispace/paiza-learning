f9e17f3b08725932aedf83c2b6dac8c2044f7144
yuki <fledgling0829@gmail.com>
2026-04-22 09:23:51 +0900

    feat: 練習コード

---
+++ components/review/30/299.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = ['2 2']
+const [N, K] = lines[0].split(' ').map(BigInt)
+
+function value(i: bigint) {
+  const r = i % 3n
+
+  if (r === 1n) return 1n
+
+  if (r === 2n) return 0n
+
+  return -1n
+}
+
+let sum = 0n
+
+for (let i = N; i <= K && i < N + 3n; i++) {
+  sum += value(i)
+}
+
+console.log(sum.toString())
+</script>
