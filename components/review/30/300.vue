35c39648f6244642760571d38a247197df6841a1
yuki <fledgling0829@gmail.com>
2026-04-23 09:23:09 +0900

    feat: 練習コード

---
+++ components/review/30/300.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const lines = [
+  '10',
+  '0 1 2 3 4 5 6 7 8 9',
+  '5',
+  '0 1',
+  '0 4',
+  '3 8',
+  '2 9',
+  '1 3',
+]
+const N = Number(lines[0])
+const A = lines[1].split(' ').map(Number)
+const q = Number(lines[2])
+
+const S: number[] = new Array(N + 1).fill(0)
+for (let i = 0; i < N; i++) {
+  S[i + 1] = S[i] + A[i]
+}
+
+let out = ''
+for (let i = 0; i < q; i++) {
+  const [l, u] = lines[3 + i].split(' ').map(Number)
+  out += (S[u + 1] - S[l]) + '\n'
+}
+
+console.log(out)
+</script>
