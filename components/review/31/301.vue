085ebabf11a02bc2170a9e307688f91f7641c799
yuki <fledgling0829@gmail.com>
2026-04-24 14:09:02 +0900

    feat: 練習コード

---
+++ components/review/31/301.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = [
+  '3 300',
+  '150',
+  '120',
+  '130',
+]
+const [N, X] = lines[0].split(' ').map(Number)
+let sum = 0
+
+for (let i = 0; i < N; i++) {
+  sum += Number(lines[1 + i])
+}
+
+console.log(sum <= X ? 'Yes' : 'No')
+</script>
