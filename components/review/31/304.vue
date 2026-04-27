b2dfe4ff8d155a2190c16a46b2d3b8dea112d362
yuki <fledgling0829@gmail.com>
2026-04-27 19:53:35 +0900

    feat: 練習コード

---
+++ components/review/31/304.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const lines = ['5']
+const N = Number(lines[0])
+
+if (N === 1 || N === 2) {
+  console.log(1)
+}
+else {
+  let a = 1
+  let b = 1
+
+  for (let i = 3; i <= N; i++) {
+    const c = (a + b) % 10000
+    a = b
+    b = c
+  }
+
+  console.log(b)
+}
+</script>
