10ef1401aea08c73e15ce43d22799f1b741fdbdf
yuki <fledgling0829@gmail.com>
2026-04-29 09:44:15 +0900

    feat: 練習コード

---
+++ components/review/31/306.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const lines = [
+  '6 5',
+  '3 5 0 2 1',
+]
+const [, k] = lines[0].split(' ').map(Number)
+let ans = 0
+
+if (k > 0) {
+  const s = lines[1].split(' ').map(Number)
+
+  for (const v of s) {
+    ans |= (1 << v)
+  }
+}
+
+console.log(ans)
+</script>
