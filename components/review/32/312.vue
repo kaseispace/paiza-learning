5333d0694578e0c31898e5d48f408fc92d24150b
yuki <fledgling0829@gmail.com>
2026-05-05 19:57:06 +0900

    feat: 練習コード

---
+++ components/review/32/312.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '1 3 5 7 9',
+]
+const n = Number(lines[0])
+const a = lines[1].split(' ').map(Number)
+
+let ok = true
+
+for (let i = 0; i < n - 1; i++) {
+  if (a[i] > a[i + 1]) {
+    ok = false
+    break
+  }
+}
+
+console.log(ok ? 'Yes' : 'No')
+</script>
