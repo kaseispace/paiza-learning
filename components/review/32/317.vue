2a42288b2f9a65df8bb3fe6f689e74919beb9228
yuki <fledgling0829@gmail.com>
2026-05-10 10:02:01 +0900

    feat: 練習コード

---
+++ components/review/32/317.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const lines = ['45 15']
+let [A, B] = lines[0].split(' ').map(Number)
+
+while (B !== 0) {
+  const r = A % B
+  A = B
+  B = r
+}
+
+console.log(A)
+</script>
