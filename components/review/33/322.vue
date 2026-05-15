2c5ef7bfd184e55ff55a2c8ec5ed06d8b390a47d
yuki <fledgling0829@gmail.com>
2026-05-15 10:27:02 +0900

    feat: 練習コード

---
+++ components/review/33/322.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const lines = ['9']
+const N = Number(lines[0])
+
+let x = N
+let count = 0
+
+while (x > 0) {
+  count += x & 1
+  x >>= 1
+}
+
+console.log(count)
+</script>
