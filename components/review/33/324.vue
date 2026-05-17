5e47038d5583a36dc2ab70c71eb9554ee9af2513
yuki <fledgling0829@gmail.com>
2026-05-17 09:55:48 +0900

    feat: 練習コード

---
+++ components/review/33/324.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '5 4 3 2 1',
+]
+const N = Number(lines[0])
+const arr = lines[1].split(' ').map(Number)
+
+const a: number[] = new Array(N + 1)
+for (let i = 1; i <= N; i++) a[i] = arr[i - 1]
+
+const pos: number[] = new Array(N + 1)
+for (let i = 1; i <= N; i++) {
+  pos[a[i]] = i
+}
+
+let swaps = 0
+
+for (let i = 1; i <= N; i++) {
+  const p = pos[i]
+
+  if (p !== i) {
+    const x = a[i]
+    const y = a[p]
+
+    a[i] = y
+    a[p] = x
+
+    pos[x] = p
+    pos[y] = i
+
+    swaps++
+  }
+}
+
+console.log(swaps)
+</script>
