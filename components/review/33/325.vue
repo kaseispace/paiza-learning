d6aa38b7b3be80fb61e09a03d921ae3799b8a31b
yuki <fledgling0829@gmail.com>
2026-05-18 09:27:41 +0900

    feat: 練習コード

---
+++ components/review/33/325.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '17',
+  '188',
+  '38',
+  '77777',
+]
+const n = Number(lines[0])
+const table: number[][] = Array.from({ length: 10 }, () => [])
+
+for (let i = 1; i <= n; i++) {
+  const x = Number(lines[i])
+  const h = x % 10
+  table[h].push(x)
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(table[i].join(' '))
+}
+</script>
