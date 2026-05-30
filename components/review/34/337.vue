f236fccc13afaf881602ca76e43c5ba3da2c02b4
yuki <fledgling0829@gmail.com>
2026-05-30 10:03:25 +0900

    feat: 練習コード

---
+++ components/review/34/337.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = ['10']
+const N = Number(lines[0])
+const MOD = 1000000007
+
+const memo: number[] = Array(N + 1).fill(-1)
+
+const fib = (n: number) => {
+  if (memo[n] !== -1) return memo[n]
+  if (n === 1 || n === 2) return memo[n] = 1
+
+  memo[n] = (fib(n - 1) + fib(n - 2)) % MOD
+  return memo[n]
+}
+
+console.log(fib(N))
+</script>
