197995359d2c6f67351661d8a922268be54add77
yuki <fledgling0829@gmail.com>
2026-06-01 09:39:50 +0900

    feat: 練習コード

---
+++ components/review/34/339.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '1 c',
+  '2',
+  '1 d',
+  '2',
+]
+const Q = Number(lines[0])
+const A: string[] = []
+
+for (let i = 1; i <= Q; i++) {
+  const parts = lines[i].split(' ')
+
+  if (parts[0] === '1') {
+    A.push(parts[1])
+  }
+  else {
+    const last = A.pop()
+    console.log(last)
+  }
+
+  console.log(A.join(' '))
+}
+</script>
