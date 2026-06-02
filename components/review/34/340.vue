063477fd499a73982c69c3e5234cad6ccd5a9850
yuki <fledgling0829@gmail.com>
2026-06-02 09:35:56 +0900

    feat: 練習コード

---
+++ components/review/34/340.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const lines = [
+  '3 2 3',
+  '1 2 1',
+  '1 3 3',
+  '+ 1',
+  '+ 2',
+  '- 1',
+]
+let p = 0
+const [N, M, Q] = lines[p++].split(' ').map(Number)
+
+const friend: number[][] = Array.from({ length: N }, () => Array(N).fill(0))
+
+for (let i = 0; i < M; i++) {
+  const [a, b, f] = lines[p++].split(' ').map(Number)
+  friend[a - 1][b - 1] = f
+  friend[b - 1][a - 1] = f
+}
+
+const inGroup: boolean[] = Array(N).fill(false)
+let groupSize = 0
+
+const calcPopularity = () => {
+  if (groupSize === 0 || groupSize === N) return 0
+
+  let best = 0
+  for (let u = 0; u < N; u++) {
+    if (!inGroup[u]) continue
+    for (let v = 0; v < N; v++) {
+      if (inGroup[v]) continue
+      if (friend[u][v] > best) best = friend[u][v]
+    }
+  }
+  return best
+}
+
+const output: number[] = []
+
+for (let i = 0; i < Q; i++) {
+  const [op, qStr] = lines[p++].split(' ')
+  const q = Number(qStr) - 1
+
+  if (op === '+') {
+    inGroup[q] = true
+    groupSize++
+  }
+  else {
+    inGroup[q] = false
+    groupSize--
+  }
+
+  output.push(calcPopularity())
+}
+
+console.log(output.join('\n'))
+</script>
