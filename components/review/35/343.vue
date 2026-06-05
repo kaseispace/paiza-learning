3abab33837102bbc9ef3d3d13c98ec6ddd0b09ff
yuki <fledgling0829@gmail.com>
2026-06-05 10:03:19 +0900

    feat: 練習コード

---
+++ components/review/35/343.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = [
+  '3 6',
+  '1 2 3',
+]
+const [, K] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+
+const INF = 1e9
+const dp: number[] = Array(K + 1).fill(INF)
+dp[0] = 0
+
+for (const x of a) {
+  for (let s = K; s >= x; s--) {
+    if (dp[s - x] + 1 < dp[s]) {
+      dp[s] = dp[s - x] + 1
+    }
+  }
+}
+
+console.log(dp[K] === INF ? -1 : dp[K])
+</script>
