0f30e0110d5db3607cd99b596e6612193479e6d7
yuki <fledgling0829@gmail.com>
2026-06-12 09:33:10 +0900

    feat: 練習コード

---
+++ components/review/35/350.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const lines = [
+  '5 5',
+  '1 2 3 4 5',
+  '1 1',
+  '1 2',
+  '2 3',
+  '2 4',
+  '3 5',
+
+]
+const [n, q] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+
+const sum: number[] = Array(n + 1).fill(0)
+
+for (let i = 0; i < n; i++) {
+  sum[i + 1] = sum[i] + a[i]
+}
+
+const answer: string[] = []
+
+for (let i = 0; i < q; i++) {
+  const [l, r] = lines[i + 2].split(' ').map(Number)
+
+  answer.push(String(sum[r] - sum[l - 1]))
+}
+
+console.log(answer.join('\n'))
+</script>
