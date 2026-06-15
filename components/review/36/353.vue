ab7780d0df9363720abda5f68884853c86ec0043
yuki <fledgling0829@gmail.com>
2026-06-15 10:14:16 +0900

    feat: 練習コード

---
+++ components/review/36/353.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  '0 1 1 0 0',
+  '1 1 0 1 1',
+  '1 0 0 0 0',
+  '0 1 0 1 1',
+  '0 1 0 1 0',
+]
+const n = Number(lines[0])
+const loops: number[] = []
+
+for (let i = 0; i < n; i++) {
+  const row = lines[i + 1].split(' ').map(Number)
+
+  if (row[i] === 1) {
+    loops.push(i + 1)
+  }
+}
+
+let out = ''
+out += loops.length + '\n'
+
+for (const v of loops) {
+  out += v + '\n'
+}
+
+console.log(out)
+</script>
