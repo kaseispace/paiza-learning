db1df50c0c00bfcdf03cd6aec60e0f3d8e242a44
yuki <fledgling0829@gmail.com>
2026-06-19 10:35:57 +0900

    feat: 練習コード

---
+++ components/review/36/357.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const lines = [
+  'aaaaaa',
+  '2',
+]
+const s = lines[0]
+const i = Number(lines[1])
+
+const start = i - 1
+
+let ans = 0
+
+while (ans < s.length - start && s[ans] === s[start + ans]) {
+  ans++
+}
+
+console.log(ans)
+</script>
