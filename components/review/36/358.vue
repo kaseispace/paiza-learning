9ecf54eaf89b36e4f1cf183246e6b0ddaaa35f31
yuki <fledgling0829@gmail.com>
2026-06-20 11:18:52 +0900

    feat: 練習コード

---
+++ components/review/36/358.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const lines = [
+  '5 4',
+  '9 12 1 29 8',
+]
+const [N, K] = lines[0].split(' ').map(Number)
+const A = lines[1].split(' ').map(Number)
+const dp: number[] = Array(N).fill(1)
+
+let ans = 1
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < i; j++) {
+    if (Math.abs(A[i] - A[j]) <= K) {
+      dp[i] = Math.max(dp[i], dp[j] + 1)
+    }
+  }
+
+  ans = Math.max(ans, dp[i])
+}
+
+console.log(ans)
+</script>
