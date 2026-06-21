1fc46e79629a018ea8121b789833f40f3c4d99be
yuki <fledgling0829@gmail.com>
2026-06-21 11:15:17 +0900

    feat: 練習コード

---
+++ components/review/36/359.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  'b',
+  'ab',
+  'a',
+  'aa',
+]
+const arr = lines.slice(1)
+
+arr.sort((a, b) => {
+  if (a.length !== b.length) {
+    return a.length - b.length
+  }
+
+  return a.localeCompare(b)
+})
+
+console.log(arr.join('\n'))
+</script>
