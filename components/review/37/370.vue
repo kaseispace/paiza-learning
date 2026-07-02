816944462ca9a54c20af2e7a39fa95c76bacc907
yuki <fledgling0829@gmail.com>
2026-07-02 10:03:14 +0900

    feat: 練習コード

---
+++ components/review/37/370.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const lines = [
+  '3',
+  '0 2 3',
+  '3 0 2',
+  '2 3 0',
+  '1 2 3',
+]
+const n = Number(lines[0])
+const cost: number[][] = []
+
+for (let i = 0; i < n; i++) {
+  cost.push(lines[i + 1].split(' ').map(Number))
+}
+
+const path = lines[n + 1].split(' ').map(Number)
+let ans = 0
+
+for (let i = 0; i < n; i++) {
+  const from = path[i] - 1
+  const to = path[(i + 1) % n] - 1
+
+  ans += cost[from][to]
+}
+
+console.log(ans)
+</script>
