2f9f49d589e3fcb7b9199612ffec2043b8755f14
yuki <fledgling0829@gmail.com>
2026-07-05 15:08:27 +0900

    feat: 練習コード

---
+++ components/review/38/373.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const lines = [
+  '3 6 5',
+  '1 3',
+  '4 2',
+  '2 3',
+]
+const [T, L, H] = lines[0].split(' ').map(Number)
+let water = H
+let ok = true
+
+for (let i = 0; i < T; i++) {
+  const [A, B] = lines[i + 1].split(' ').map(Number)
+
+  water = Math.min(L, water + A)
+
+  if (water < B) {
+    ok = false
+    break
+  }
+
+  water -= B
+}
+
+console.log(ok ? 'Yes' : 'No')
+</script>
