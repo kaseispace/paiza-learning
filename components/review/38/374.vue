dac75a426cf28368aee2040404de86c6fd23d481
yuki <fledgling0829@gmail.com>
2026-07-06 14:23:15 +0900

    feat: 練習コード

---
+++ components/review/38/374.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const lines = [
+  '7 0',
+  '1 0 0 1 1 0 1',
+]
+const [N, L] = lines[0].split(' ').map(Number)
+const B = lines[1].split(' ').map(Number)
+
+let left = 0
+let workDays = 0
+let ans = 0
+
+for (let right = 0; right < N; right++) {
+  if (B[right] === 0) {
+    workDays++
+  }
+
+  while (workDays > L) {
+    if (B[left] === 0) {
+      workDays--
+    }
+
+    left++
+  }
+
+  ans = Math.max(ans, right - left + 1)
+}
+
+console.log(ans.toString())
+</script>
