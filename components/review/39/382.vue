8947c71b9c688d20599d87c9ca7a896e4d72b976
yuki <fledgling0829@gmail.com>
2026-07-14 13:44:02 +0900

    feat: 練習コード

---
+++ components/review/39/382.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const lines = [
+  '3 17',
+  '5 4',
+  '7 6',
+  '2 1',
+]
+const [N, W] = lines[0].split(' ').map(Number)
+const dp: number[] = Array(W + 1).fill(0)
+
+for (let i = 0; i < N; i++) {
+  const [weight, value] = lines[i + 1].split(' ').map(Number)
+
+  for (let w = weight; w <= W; w++) {
+    dp[w] = Math.max(dp[w], dp[w - weight] + value)
+  }
+}
+
+console.log(dp[W])
+</script>
