baa6e2d83f2c4e9d1903fec9482c454395d20620
yuki <fledgling0829@gmail.com>
2026-07-18 10:32:26 +0900

    feat: 練習コード

---
+++ components/review/39/386.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const lines = [
+  '8 300',
+  '70',
+  '90',
+  '40',
+  '90',
+  '30',
+  '10',
+  '100',
+  '30',
+]
+const [N, K] = lines[0].split(' ').map(Number)
+
+let count = 1
+let sum = 0
+
+for (let i = 0; i < N; i++) {
+  const weight = Number(lines[i + 1])
+
+  if (sum + weight <= K) {
+    sum += weight
+  }
+  else {
+    count++
+    sum = weight
+  }
+}
+
+console.log(count)
+</script>
