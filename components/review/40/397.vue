19c7801e96f617b7d05fe9bd9d5396c071281073
yuki <fledgling0829@gmail.com>
2026-07-29 09:37:53 +0900

    feat: 練習コード

---
+++ components/review/40/397.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const lines = [
+  '5 2',
+  '1 5',
+  '2 3',
+]
+const [N, Q] = lines[0].split(' ').map(Number)
+const imos: number[] = Array(N + 2).fill(0)
+
+for (let i = 0; i < Q; i++) {
+  const [L, R] = lines[i + 1].split(' ').map(Number)
+
+  imos[L]++
+  imos[R + 1]--
+}
+
+let ans = 0
+let sum = 0
+
+for (let i = 1; i <= N; i++) {
+  sum += imos[i]
+  if (sum % 2 === 1) ans++
+}
+
+console.log(ans)
+</script>
