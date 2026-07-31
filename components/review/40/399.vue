b7bbb2ed4782f543c0b0cc9da775de445d9c54bb
yuki <fledgling0829@gmail.com>
2026-07-31 14:57:59 +0900

    feat: 練習コード

---
+++ components/review/40/399.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const lines = ['2 5 7']
+const [a, b, m]: number[] = lines[0].split(' ').map(Number)
+
+let A: number = a
+let B: number = b
+const M: number = m
+
+let ans = 1 % M
+A %= M
+
+while (B > 0) {
+  if (B & 1) {
+    ans = (ans * A) % M
+  }
+
+  A = (A * A) % M
+  B >>= 1
+}
+
+console.log(ans)
+</script>
