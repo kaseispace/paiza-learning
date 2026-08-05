bef5279b941c041ab103db8035657be250a5cc3e
yuki <fledgling0829@gmail.com>
2026-08-05 11:39:49 +0900

    feat: 練習コード

---
+++ components/review/41/404.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = [
+  '3',
+  '2',
+  '4',
+  '5',
+
+]
+const N = Number(lines[0])
+const list: number[] = []
+
+for (let i = 1; i <= N; i++) {
+  list.unshift(Number(lines[i]))
+}
+
+console.log(list.join('\n'))
+</script>
