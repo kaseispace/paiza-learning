e78abfbfc53240eaedc5f4501b58899abfa0b93c
yuki <fledgling0829@gmail.com>
2026-08-08 09:57:45 +0900

    feat: 練習コード

---
+++ components/review/41/407.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const lines = [
+  '6 4',
+  'kirishima rokumura midorikawa kurota neko margaret',
+  'noda kino sakura nitta',
+  '3 4 2 8',
+]
+const [, M] = lines[0].split(' ').map(Number)
+const A = lines[1].split(' ')
+const B = lines[2].split(' ')
+const X = lines[3].split(' ').map(Number)
+
+const queue = [...A]
+
+for (let i = 0; i < M; i++) {
+  queue.splice(X[i] - 1, 0, B[i])
+}
+
+console.log(queue.join('\n'))
+</script>
