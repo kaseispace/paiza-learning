9b8cd28644ad29b69c6c4e8c62e510aa335d2ac6
yuki <fledgling0829@gmail.com>
2026-08-09 20:54:32 +0900

    feat: 練習コード

---
+++ components/review/41/408.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const lines = [
+  '6 8',
+  '4 6 5 1 5 3',
+]
+const [N, K] = lines[0].split(' ').map(Number)
+const A = lines[1].split(' ').map(Number)
+
+A.sort((a, b) => a - b)
+
+let left = 0
+let right = N - 1
+let answer = 0
+
+while (left < right) {
+  if (A[left] + A[right] >= K) {
+    answer++
+    left++
+    right--
+  }
+  else {
+    left++
+  }
+}
+
+console.log(answer)
+</script>
