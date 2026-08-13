9b456850e547ef6809b37cae262e59743f801c6e
yuki <fledgling0829@gmail.com>
2026-08-13 16:04:40 +0900

    feat: 練習コード

---
+++ components/review/42/412.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const lines = [
+  '3',
+  '1 -4 -4',
+]
+const n = Number(lines[0])
+const a = lines[1].split(' ').map(Number)
+
+let count = 0
+
+for (let i = 0; i < n - 1; i++) {
+  if (a[i] === a[i + 1]) {
+    count++
+  }
+}
+
+console.log(count)
+</script>
