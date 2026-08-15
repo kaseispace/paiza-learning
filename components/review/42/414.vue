078d6c20d65586ac5489fbc5cd4477e17493f952
yuki <fledgling0829@gmail.com>
2026-08-15 10:10:42 +0900

    feat: 練習コード

---
+++ components/review/42/414.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const lines = [
+  '6 9 E',
+  'R',
+]
+const [Y, X, D] = lines[0].split(' ') as [number, number, string]
+const d = lines[1]
+
+let y = Y
+let x = X
+
+if (D === 'N') {
+  if (d === 'L') {
+    x--
+  }
+  else {
+    x++
+  }
+}
+else if (D === 'S') {
+  if (d === 'L') {
+    x++
+  }
+  else {
+    x--
+  }
+}
+else if (D === 'E') {
+  if (d === 'L') {
+    y--
+  }
+  else {
+    y++
+  }
+}
+else if (d === 'L') {
+  y++
+}
+else {
+  y--
+}
+
+console.log(y, x)
+</script>
