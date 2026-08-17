5b8b4eb908ddb856a183830c818f5013351940ad
yuki <fledgling0829@gmail.com>
2026-08-17 13:23:55 +0900

    feat: 練習コード

---
+++ components/review/42/416.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const lines = [
+  '4 200000000000',
+  '1000000 1000000 1000000 1000000',
+]
+const [n, x] = lines[0].split(' ').map(Number)
+const a = lines[1].split(' ').map(Number)
+
+a.sort((p, q) => q - p)
+
+let sum = 0
+
+for (let i = 0; i < n; i++) {
+  sum += a[i]
+
+  if (sum >= x) {
+    console.log(i + 1)
+    process.exit(0)
+  }
+}
+
+console.log(-1)
+</script>
