022ec9316cbb9d9ca2708030e36d28c65212a4fa
yuki <fledgling0829@gmail.com>
2026-08-18 14:40:03 +0900

    feat: 練習コード

---
+++ components/review/42/417.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '0 0',
+  '2 2',
+  '-1 1',
+  '1 -1',
+]
+const n = Number(lines[0])
+const [x0, y0] = lines[1].split(' ').map(Number)
+
+let nearestCity = 1
+let minDistanceSquared = Infinity
+
+for (let i = 1; i < n; i++) {
+  const [x, y] = lines[i + 1].split(' ').map(Number)
+
+  const dx = x - x0
+  const dy = y - y0
+  const distanceSquared = dx * dx + dy * dy
+
+  if (distanceSquared < minDistanceSquared) {
+    minDistanceSquared = distanceSquared
+    nearestCity = i
+  }
+}
+
+console.log(nearestCity)
+</script>
