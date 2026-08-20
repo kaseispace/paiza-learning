f727cb7a1be1fe54fb58de55780655ce7af2c85c
yuki <fledgling0829@gmail.com>
2026-08-20 13:40:54 +0900

    feat: 練習コード

---
+++ components/review/42/419.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const lines = [
+  '4 8',
+  '5 3 8 2',
+]
+const firstLine = lines[0].split(' ').map(Number)
+const n = firstLine[0]
+const k = firstLine[1]
+
+const a = lines[1].split(' ').map(Number)
+
+const dp: number[] = Array(k + 1).fill(0)
+dp[0] = 1
+
+for (let i = 0; i < n; i++) {
+  const eggs = a[i]
+
+  for (let sum = k; sum >= eggs; sum--) {
+    dp[sum] += dp[sum - eggs]
+  }
+}
+
+console.log(dp[k])
+</script>
