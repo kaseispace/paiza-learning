8569cc4fca9d55bbc584ed076fc2c5285446b553
yuki <fledgling0829@gmail.com>
2026-08-21 11:32:40 +0900

    feat: 練習コード

---
+++ components/review/42/420.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '1 1',
+  '1 4',
+  '1 2',
+  '2',
+
+]
+const Q = Number(lines[0])
+const queue: number[] = []
+const result: string[] = []
+
+for (let i = 0; i < Q; i++) {
+  const query = lines[i + 1].split(' ').map(Number)
+
+  if (query[0] === 1) {
+    const x = query[1]
+    queue.push(x)
+  }
+  else {
+    queue.shift()
+  }
+
+  result.push(queue.join(' '))
+}
+
+process.stdout.write(result.join('\n') + '\n')
+</script>
