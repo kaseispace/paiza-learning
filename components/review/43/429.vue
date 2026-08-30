f5e06fb0eb85edd36724627f3030c19f0bf74812
yuki <fledgling0829@gmail.com>
2026-08-30 15:50:24 +0900

    feat: 練習コード

---
+++ components/review/43/429.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const lines = [
+  '2 3',
+  '3 -2',
+]
+const [a1, a2] = lines[0].split(' ').map(Number)
+const [b1, b2] = lines[1].split(' ').map(Number)
+
+const dotProduct = a1 * b1 + a2 * b2
+
+if (dotProduct === 0) {
+  console.log('Yes')
+}
+else {
+  console.log('No')
+}
+</script>
