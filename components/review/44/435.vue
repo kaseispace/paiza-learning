c8b7f26cdb1b86399d9fcf7e9034c8f470cfdfa5
yuki <fledgling0829@gmail.com>
2026-09-05 10:11:20 +0900

    feat: 練習コード

---
+++ components/review/44/435.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const lines = [
+  '8734',
+  '23128',
+]
+const K = Number(lines[0])
+const N = Number(lines[1])
+
+const MOD = 10000
+
+const cumulative: number[] = Array(N + 1).fill(0)
+
+let answer = 0
+
+for (let i = 1; i <= N; i++) {
+  let current: number
+
+  if (i <= K) {
+    current = 1
+  }
+  else {
+    current = (cumulative[i - 1] - cumulative[i - K - 1] + MOD) % MOD
+  }
+
+  cumulative[i] = (cumulative[i - 1] + current) % MOD
+
+  if (i === N) {
+    answer = current
+  }
+}
+
+console.log(answer)
+</script>
