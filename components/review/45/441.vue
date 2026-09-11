7b79b5af6e233cb4926f7bde7ec7596a4efda8e9
yuki <fledgling0829@gmail.com>
2026-09-11 15:17:10 +0900

    feat: 練習コード

---
+++ components/review/45/441.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const lines = [
+  '7',
+  '7',
+  '7',
+  '7',
+  '7',
+  '7',
+  '7',
+  '7',
+]
+const N = Number(lines[0])
+const scores: number[] = []
+
+for (let i = 0; i < N; i++) {
+  scores.push(Number(lines[i + 1]))
+}
+
+const sum = scores.reduce((total, score) => total + score, 0)
+const average = sum / N
+
+let answer = 0
+
+for (const score of scores) {
+  answer += Math.max(score, average)
+}
+
+answer /= N
+
+console.log(answer.toFixed(10))
+</script>
