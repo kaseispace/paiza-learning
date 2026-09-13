92ff4a8b65f274066fb5d41cff93bd7c4612a7b9
yuki <fledgling0829@gmail.com>
2026-09-13 10:49:25 +0900

    feat: 練習コード

---
+++ components/review/45/443.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const lines = [
+  '5',
+  'a8 BK',
+  'g6 WP',
+  'c3 BR',
+  'e7 BK',
+  'd2 WP',
+
+]
+const N = Number(lines[0])
+const board: string[][] = Array.from({ length: 8 }, () => Array(8).fill('--'))
+
+for (let i = 0; i < N; i++) {
+  const [position, piece] = lines[i + 1].split(' ')
+
+  const column = position.charCodeAt(0) - 'a'.charCodeAt(0)
+  const rank = Number(position[1])
+
+  const row = 8 - rank
+
+  board[row][column] = piece
+}
+
+for (let row = 0; row < 8; row++) {
+  console.log(board[row].join(' '))
+}
+</script>
