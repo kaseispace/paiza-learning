8beb12cd9837b4fb5a64a22a830aa74c0f564cd8
yuki <fledgling0829@gmail.com>
2026-09-19 16:18:52 +0900

    feat: 練習コード

---
+++ components/review/45/449.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '0 2 0 0',
+  '2 0 0 0',
+  '0 0 0 1',
+  '0 0 1 0',
+]
+const N = Number(lines[0])
+let edgeCount = 0
+
+for (let i = 0; i < N; i++) {
+  const row = lines[i + 1].split(' ').map(Number)
+
+  for (let j = i + 1; j < N; j++) {
+    edgeCount += row[j]
+  }
+}
+
+console.log(edgeCount)
+</script>
