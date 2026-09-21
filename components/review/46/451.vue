fefb7e397e550fa0637914b8fb8f018e1127df1c
yuki <fledgling0829@gmail.com>
2026-09-21 10:47:45 +0900

    feat: 練習コード

---
+++ components/review/46/451.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const lines = [
+  '2',
+  '1 1',
+  '1 2',
+  '2 3',
+  '2 4',
+]
+const n = Number(lines[0])
+const arrivals: number[] = []
+const shipments: number[] = []
+
+for (let i = 0; i < 2 * n; i++) {
+  const [type, time] = lines[i + 1].split(' ').map(Number)
+
+  if (type === 1) {
+    arrivals.push(time)
+  }
+  else {
+    shipments.push(time)
+  }
+}
+
+arrivals.sort((a, b) => a - b)
+shipments.sort((a, b) => a - b)
+
+let answer = 0
+
+for (let i = 0; i < n; i++) {
+  answer += Math.max(0, shipments[i] - arrivals[i])
+}
+
+console.log(answer)
+</script>
