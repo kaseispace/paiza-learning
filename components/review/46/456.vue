b4bc9ea897105f5d10fd5069e7ac9ab80bd69c28
yuki <fledgling0829@gmail.com>
2026-09-26 19:47:40 +0900

    feat: 練習コード

---
+++ components/review/46/456.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const lines = [
+  '4',
+  '1 4 2 3',
+]
+const n = Number(lines[0])
+const a = lines[1].split(' ').map(Number)
+
+const result: number[] = []
+
+for (let i = 0; i < n; i++) {
+  result.push(a[a[i] - 1])
+}
+
+console.log(result.join(' '))
+</script>
